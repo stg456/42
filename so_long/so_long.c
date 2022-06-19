e412743ebdbff09803c60128afb047022117eff9
stgerard <stgerard@student.42mulhouse.fr>
2022-06-19 17:30:22 +0200

    NEW START

---
+++ so_long/so_long.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/19 17:23:07 by stgerard          #+#    #+#             */
+/*   Updated: 2022/06/19 17:29:36 by stgerard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+#include <mlx.h>
+#include <stdio.h>
+
+int	close_hook(t_env *e)
+{
+	//mlx_destroy_image(e->mlx, e->img);
+	mlx_destroy_window(e->mlx, e->win);
+	e->win = NULL;
+	exit(EXIT_SUCCESS);
+}
+
+int	key_hook(int key, t_env *e)
+{
+	if (key == 53)
+		close_hook(e);
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	t_env	e;
+	char	*str;
+	(void)argc;
+	//(void)argv;
+
+	str = NULL;
+	e.mlx = mlx_init();
+	e.win = mlx_new_window(e.mlx, 1000, 500, "so_long stgerard");
+	mlx_hook(e.win, 17, 0, &close_hook, &e);
+	mlx_key_hook(e.win, &key_hook, &e);
+	read_map(argv);
+
+	//mlx_pixel_put(e.mlx, e.win, 0, 0, 0xffffffff);
+	//mlx_string_put(e.mlx, e.win, 50, 50, 0xffffffff, line);
+
+	mlx_loop(e.mlx);
+	return (0);
+}
---
+++ so_long/so_long.h
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/19 17:22:41 by stgerard          #+#    #+#             */
+/*   Updated: 2022/06/19 17:29:36 by stgerard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <mlx.h>
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <stdarg.h>
+# include <sys/types.h>
+# include "./libft/libft.h"
+# define BUFFER_SIZE 3000
+
+// struct mlx et window
+typedef struct s_env
+{
+	void	*mlx;
+	void	*win;
+	void	*img;
+	int		nbline;
+	int		x;
+	int		y;
+	int		z;
+	int		**map;
+}				t_env;
+
+typedef struct s_draw
+{
+	int		startx;
+	int		starty;
+	int		endxx;
+	int		endxy;
+	int		endyx;
+	int		endyy;
+}				t_draw;
+
+// initilisation
+
+int		read_map(char **argv);
+int		draw_map(int **map);
+
+// utils
+char	**ft_split(char const *s, char c);
+size_t	ft_strlen(const char *str);
+int		ft_atoi(const char *str);
+//char	*ft_strchr(char *s, int c);
+//char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+
+#endif
