32b9cca6680344acfbc5d58aefb8b8d81879b23a
stgerard <stgerard@student.42mulhouse.fr>
2022-06-27 11:11:00 +0200

    ip

--- so_long3/check_map.c
+++ so_long3/check_map.c
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 13:59:55 by stgerard          #+#    #+#             */
-/*   Updated: 2022/06/23 16:50:13 by stgerard         ###   ########.fr       */
+/*   Updated: 2022/06/27 11:10:50 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,17 +33,17 @@ void	wall_check(t_env *e)
 
 }
 
-// void	min_check(t_env *map)
-// {
-// 	if (map->p_nb != 1)
-// 		perror("\x1B[31mError : map file must contain one player position.");
-// 	if (map->c_nb < 1)
-// 		ft_printf("\x1B[31mError : map file must contain one collectible.");
-// 	if (map->e_nb < 1)
-// 		ft_printf("\x1B[31mError : map file must contain one exit.");
-// 	if (map->p_nb != 1 || map->c_nb < 1 || map->e_nb < 1)
-// 		exit(EXIT_FAILURE);
-// }
+void	min_check(t_env *map)
+{
+	if (map->p_nb != 1)
+		perror("\x1B[31mError : map file must contain one player position.");
+	if (map->c_nb < 1)
+		ft_printf("\x1B[31mError : map file must contain one collectible.");
+	if (map->e_nb < 1)
+		ft_printf("\x1B[31mError : map file must contain one exit.");
+	if (map->p_nb != 1 || map->c_nb < 1 || map->e_nb < 1)
+		exit(EXIT_FAILURE);
+}
 
 int	check(t_env *e)
 {
