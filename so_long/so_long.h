c2ec2f08a534cd8b4992ab19829aafdb1ce40fda
stgerard <stgerard@student.42mulhouse.fr>
2022-06-27 14:00:10 +0200

    bre

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 17:22:41 by stgerard          #+#    #+#             */
-/*   Updated: 2022/06/23 16:18:10 by stgerard         ###   ########.fr       */
+/*   Updated: 2022/06/27 13:53:14 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,20 +29,17 @@
 # define IMG_W 32
 # define IMG_H 32
 
-// struct map
-typedef struct s_map
-{
-	char	**map;
-	void	*objet;
-	int		x;
-	int		y;
-	int		diamonds;
-}				t_map;
+# define ESC 53
+# define W 13
+# define A 0
+# define S 1
+# define D 2
 
 // struct mlx et window
 typedef struct s_env
 {
 	void	*mlx;
+	char	*map;
 	void	*win;
 	int		size_x;
 	int		size_y;
@@ -50,22 +47,25 @@ typedef struct s_env
 	int		p_y;
 	int		counter;
 	int		collected;
-	t_map	*map;
+
 }				t_env;
 
 // initilisation
 
+void	ft_error(char *str);
+int		check(t_env *e);
 void	ft_size_win(t_env *e, char **argv);
-void	ft_init(t_map map, t_env e);
-//int		read_map(char **argv);
-//int		draw_map(int **map);
+char	*init_map(char **argv, t_env *e);
+int		key_hook(int key, t_env *e);
+int		close_hook(t_env *e);
 
 // utils
+
 void	*ft_calloc(size_t count, size_t size);
-char	**ft_split(char const *s, char c);
-size_t	ft_strlen(const char *str);
-int		ft_atoi(const char *str);
-char	*ft_strchr(char *s, int c);
+//char	**ft_split(char const *s, char c);
+//size_t	ft_strlen(const char *str);
+//int		ft_atoi(const char *str);
+//char	*ft_strchr(char *s, int c);
 //char	*ft_strjoin(char *s1, char *s2);
 char	*get_next_line(int fd);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
