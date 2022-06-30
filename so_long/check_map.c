e8d5a5fd07bace3dfc2ba0e2b412c6b55a4835a9
stgerard <stgerard@student.42mulhouse.fr>
2022-06-30 18:13:20 +0200

    eth

--- so_long/check_map.c
+++ so_long/check_map.c
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 13:59:55 by stgerard          #+#    #+#             */
-/*   Updated: 2022/06/30 18:01:22 by stgerard         ###   ########.fr       */
+/*   Updated: 2022/06/30 18:13:14 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ static void	wall_check(t_env *e)
 	//e->size_x = i;
 	while (str[i] != '\0')
 	{
-		while (str[i] != '\n')
+		if (str[i] != '\n')// pas bon
 		{
 			if (str[i--] != '1' || str[i++] != '1')
 				ft_error("\x1B[31mInvalid map\n");
