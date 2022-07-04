2c510617a0cdfd8d0bc1d96aa3c0722c4996ec98
stgerard <stgerard@student.42mulhouse.fr>
2022-07-04 15:59:46 +0200

    5g5t

--- so_long/init.c
+++ so_long/init.c
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 11:21:16 by stgerard          #+#    #+#             */
-/*   Updated: 2022/07/04 12:05:59 by stgerard         ###   ########.fr       */
+/*   Updated: 2022/07/04 15:59:21 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,15 +43,20 @@ char	**map_read(char *filename)
 
 char	**init_map(char **argv, t_env *e)
 {
-	// e->mlx = NULL;
-	// e->map = NULL;
-	// if (ft_strnstr(argv[1], ".ber", ft_strlen(argv[1])) == NULL)
-	// 	ft_error("\x1B[31mError: map has to be .ber\n");
-	// fd = open(argv[1], O_RDONLY);
-	// if (fd < 0)
-	// 	ft_error("\x1B[31mInvalid map\n");
+	int	fd;
+
+	e->mlx = NULL;
+	e->map = NULL;
+	if (ft_strnstr(argv[1], ".ber", ft_strlen(argv[1])) == NULL)
+		ft_error("\x1B[31mError: map has to be .ber\n");
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		close(fd);
+		ft_error("\x1B[31mInvalid map\n");
+	}
 	e->map = map_read(argv[1]);
 	check(e);
-	// close(fd);
+	close(fd);
 	return (e->map);
 }
