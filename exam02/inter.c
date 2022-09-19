27db7f84bb998b993b309b0280e0c62d30d23627
stgerard <stgerard@student.42mulhouse.fr>
2022-09-19 16:58:06 +0200

    uykry

--- exam02/inter.c
+++ exam02/inter.c
@@ -2,54 +2,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int check(char **av, char *s)
-{
-	int i = 0;
-	int k = 0;
+// int check(char *str, char c)
+// {
+// 	int i = 0;
 
-	while (s[k] && av[1][i])
-	{
-		if (av[1][i] != s[k])
-		{
-			k++;
-		}
-		else
-			return (0);
-	}
-	return (1);
-}
-
-// pas encore bon
+// }
 
 int main(int ac, char **av)
 {
 	int i = 0;
 	int j = 0;
-	char s[] = av[1];
-	int k = 0;
-	printf("1 %s\n", s);
+	int z = 0;
+	char str[10000];
+	str[0] = "a";
 
-	if (ac != 3)
+	if (ac == 3)
 	{
 		while (av[1][i])
 		{
-			while (av[2][j])
+			if (av[1][i] == av[2][j])
 			{
-				if (av[1][i] == av[2][j])
+				while (str[z])
 				{
-					(check(*s, *av[1]) = 0)? ;
-					s[k] = av[1][i];
-					write(1, &av[1][i], 1);
-					i++;
-					k++;
+					if (str[z] != av[1][i])
+					{
+						z++;
+					}
+					else if (str[z] == av[1][i])
+					{
+					
+					}
+					str[z] = av[1][i];
 				}
-				else
-					j++;
 			}
-			i++;
 		}
-	}
-	printf("2 %s\n", s);
+	}	
 	write(1, "\n", 1);
 	return (0);
 }
+
+// pas encore bon
