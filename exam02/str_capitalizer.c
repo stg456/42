ba0a6dd6e15f2a89edf9c7cb83e2e38742f82e1d
stgerard <stgerard@student.42mulhouse.fr>
2022-09-22 14:57:03 +0200

    thrsjd

--- exam02/str_capitalizer.c
+++ exam02/str_capitalizer.c
@@ -2,21 +2,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int ac, char **av)
+void str_cap(char *str)
 {
 	int i = 0;
-	char c;
+
+	if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] -= 32;
+	write(1, &str[i], 1);
+	i++;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		else if (str[i] >= 'a' && str[i] <= 'z' && (str[i - 1] == ' ')
+		|| str[i - 1] == '\t')
+			str[i] -= 32;
+		write(1, &(str[i]), 1);
+		i++;
+	}
+}
+
+int main(int ac, char **av)
+{
+	int i = 1;
 
 	if (ac > 1)
 	{
-		while (av)
+		while (i < ac)
 		{
-			if (*av[i] - 1 != ' ' || *av[i] - 1 != '\t' || *av[i] - 1 != '\0')
-			{
-				
-			}
+			str_cap(av[i]);
+			i++;
 		}
 	}
 	write(1, "\n", 1);
 	return (0);
 }
+// pas encore bon
