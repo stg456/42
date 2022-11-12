2f51dafa389b715e0ed39c67d3633dfb713ee9d6
stgerard <stgerard@student.42mulhouse.fr>
2022-11-12 17:26:21 +0100

    i46ujhd

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 14:47:48 by stgerard          #+#    #+#             */
-/*   Updated: 2022/11/12 17:22:18 by stgerard         ###   ########.fr       */
+/*   Updated: 2022/11/12 17:25:48 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,23 @@ void	*gestphilo(t_philo *philo)
 	printf("debut routine.\n");
 	while (philo->id <= philo->rul->nb_philo)
 	{
-		// take forks
-		// eat
-		// sleep
-		// think
-		// dead
+		// si fork 1 et 2 > eat
+		// mutex lock
+		// tmps eat ft_wait 
+		// fin eat 
+		// mitex unlock
+
+
+		// pthread_mutex_lock(&philo);
+		// if (philo.forks[i] & philo.forks[i + 1])
+		// {
+		// 	pthread_mutex_lock(&philo);
+		// 	philo.forks[i];
+
+		// 	// time
+		// 	printf("philo %d ", philo.id);
+		// 	pthread_mutex_unlock(&philo);
+		// }
 	}
 
 	printf("fin routine.\n");
@@ -34,7 +46,7 @@ int	main(int argc, char **argv)
 {
 	t_philo		*philo;
 	t_rules		*rules;
-	
+	// t_chrono	*chrono;
 
 	init_arg(argc, argv, rules);
 
