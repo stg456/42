0cf8f9ca6f28bfdd13e1d1c9a06bf93901d5c648
stgerard <stgerard@student.42mulhouse.fr>
2022-11-15 14:19:13 +0100

    jdhtdrg

--- exam02/tab_mult2.c
+++ exam02/tab_mult2.c
@@ -7,23 +7,25 @@ int ft_atoi(char *str)
 	int i = 0;
 	int res = 0;
 
-	while (str[i])
+	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (str[i] >= '0' && str[i] <= '9')
-		{
-			res = res * 10 + str[i] - '0';
-			i++;
-		}
+		res = res * 10 + str[i] - '0';
+		i++;
 	}
 	printf("le atoi %d", res);
 	return (res);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
 
 void putnbr(int nb)
 {
 	if (nb > 9)
 		putnbr(nb / 10);
-	putnbr(nb % 10 + '0');
+	ft_putchar(nb % 10 + '0');
 }
 
 void tm(int b)
