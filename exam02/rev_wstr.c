89ffd362f357398efd1a06e815096a329ed2ef23
stgerard <stgerard@student.42mulhouse.fr>
2022-11-15 15:21:02 +0100

    8kjh

--- exam02/rev_wstr.c
+++ exam02/rev_wstr.c
@@ -2,43 +2,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int space(char c)
-{
-	if (c == ' ' || c == '\t')
-		return (1);
-	return (0);
-}
-
-void rev_wstr(char *str)
-{
-	int i = 0;
-	int start;
-	int end;
-
-	while (str[i])
-		i++;
-	while (i >= 0)
-	{
-		while (str[i] && (space(str[i]) == 1))
-			i--;
-		end = i;
-		while (str[i] && (space(str[i]) == 0))
-			i--;
-		start = i;
-		while (start <= end)
-		{
-			write(1, &str[i], 1);
-			start++;
-		}
-		start = i;
-		i--;
-	}
-}
-
 int main(int ac, char **av)
 {
+	int i = 0;
+	int end = 0;
+	int start = 0;
+
 	if (ac == 2)
-		rev_wstr(av[1]);
+	{
+		while (av[1][i])
+			i++;
+		while (i >= 0)
+		{
+			while (av[1][i] == ' ' || av[1][i] == '\t' || av[1][i] == '\0')
+				i--;
+			end = i;
+			while (av[1][i] != ' ' && av[1][i] != '\t' && av[1][i] != '\0')
+				i--;
+			start = i + 1;
+			while (start < end)
+			{
+				write(1, &av[1][i], 1);
+				start++;
+			}
+			i--;
+		}
+	}
 	write(1, "\n", 1);
 	return (0);
 }
