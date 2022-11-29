416a919ec495845dee61e59ae736150bbe15bcde
stgerard <stgerard@student.42mulhouse.fr>
2022-11-29 14:48:32 +0100

    8iu765

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 14:47:48 by stgerard          #+#    #+#             */
-/*   Updated: 2022/11/29 12:11:36 by stgerard         ###   ########.fr       */
+/*   Updated: 2022/11/29 14:47:25 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,21 @@ void	*gestphilo(void *ptr)
 	t_rules	rules;
 	int		i;
 
-
 	philo = (t_philo *)ptr;
 	rules = *philo->rules;
-	i = 0;
+	i = 1;
+	philo->id = 0;
 	printf("debut routine.\n");
 	// timestamp
-	while (philo->id <= philo->rules->nb_philo && philo->rules->nb_eat)
+	while (philo->id++ <= philo->rules->nb_philo && philo->rules->nb_eat)
 	{
 		while (i % 2 <= rules.nb_philo && rules.nb_eat > 0)
 		{
-			// has taken a fork
+			ft_print(philo, "FORK"); // has taken a fork pb
 			eating(rules, philo);
 			i++;
 		}
-		philo->rules->nb_eat--;
-
-
+		// philo->rules->nb_eat--;
 	}
 	printf("fin routine.\n");
 	return (NULL);
@@ -44,7 +42,6 @@ int	main(int argc, char **argv)
 {
 	t_philo		*philo;
 	t_rules		rules;
-	// t_chrono	*chrono;
 
 	philo = malloc(sizeof(t_philo));
 	if (!philo)
@@ -58,7 +55,7 @@ int	main(int argc, char **argv)
 		return (1);
 	init_mutex(philo);
 	init_thread(philo);
-	// closephilo(philo);
+	closephilo(philo);
 
 	return (0);
 }
