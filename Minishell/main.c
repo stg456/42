92fc704bda2dad229efc8997cc0c8dc9527b36c8
stgerard <stgerard@student.42mulhouse.fr>
2022-12-08 20:02:33 +0100

    ujthtrg

--- Minishell/main.c
+++ Minishell/main.c
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 19:11:00 by stgerard          #+#    #+#             */
-/*   Updated: 2022/12/08 19:14:12 by stgerard         ###   ########.fr       */
+/*   Updated: 2022/12/08 20:02:27 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,16 @@ int	main(intac, char **av)
 {
 
 }
+
+/*
+
+char *getcwd(char *buf, size_t size);  copie le chemin d'accès absolu du répertoire de travail courant dans la chaîne pointée par buf, qui est de longueur size
+int unlink(const char *pathname); unlink() détruit un nom dans le système de fichiers. Si ce nom était le dernier lien sur un fichier, et si aucun processus n'a ouvert ce fichier, ce dernier est effacé
+pid_t fork(void);
+nt execve(const char *fichier, char *const argv[],
+int dup(int oldfd);
+int dup2(int oldfd, int newfd)
+
+
+
+*/
