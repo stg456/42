ccf04ea3a264f211b4805bbb84963692e5cc48e3
stgerard <stgerard@student.42mulhouse.fr>
2022-12-09 11:04:11 +0100

    oikuj

--- Minishell/main.c
+++ Minishell/main.c
@@ -6,11 +6,11 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 19:11:00 by stgerard          #+#    #+#             */
-/*   Updated: 2022/12/08 20:03:26 by stgerard         ###   ########.fr       */
+/*   Updated: 2022/12/09 10:30:21 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define "minishell.h"
+#include "minishell.h"
 
 int	main(int ac, char **av)
 {
@@ -22,7 +22,7 @@ int	main(int ac, char **av)
 char *getcwd(char *buf, size_t size);  copie le chemin d'accès absolu du répertoire de travail courant dans la chaîne pointée par buf, qui est de longueur size
 int unlink(const char *pathname); unlink() détruit un nom dans le système de fichiers. Si ce nom était le dernier lien sur un fichier, et si aucun processus n'a ouvert ce fichier, ce dernier est effacé
 pid_t fork(void);
-nt execve(const char *fichier, char *const argv[],
+int execve(const char *fichier, char *const argv[],
 int dup(int oldfd);
 int dup2(int oldfd, int newfd)
 
