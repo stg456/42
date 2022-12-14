8e6c3ff005ea4f25d61558497959a82d33e6668f
stgerard <stgerard@student.42mulhouse.fr>
2022-12-14 19:21:10 +0100

    ujhrtg

---
+++ exam03/ft_printf.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+int	ft_printf(const char *format, ...)
+{
+	int length = 0;
+
+	va_list pointer;
+	va_start (pointer, format);
+
+	while ()
+	{
+	
+	}
+	return (va_end(pointer, length));
+}
