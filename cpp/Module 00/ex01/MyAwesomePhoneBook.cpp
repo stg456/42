3cb16d5cd1fdd4fe17b42d25a83b543b9a87ccf2
stgerard <stgerard@student.42mulhouse.fr>
2023-01-16 18:07:03 +0100

    mngfd

--- cpp/Module 00/ex01/MyAwesomePhoneBook.cpp
+++ cpp/Module 00/ex01/MyAwesomePhoneBook.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   MyAwesomePhoneBook.cpp                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/15 15:17:48 by stgerard          #+#    #+#             */
-/*   Updated: 2023/01/16 17:30:53 by stgerard         ###   ########.fr       */
+/*   Created: 2023/01/16 18:04:14 by stgerard          #+#    #+#             */
+/*   Updated: 2023/01/16 18:06:58 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,8 @@
 #include <string>
 #include <locale>
 
-int main (int argc, char **argv)
+int	main()
 {
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else if (argc > 1)
-	{
-		for (size_t i = 1; argv[i]; i++)
-		{
-			std::string	str(argv[i]); 
-			for (size_t j = 0; j < str.length(); j++)
-			{
-				str[j] = std::toupper(str[j]);
-			}
-			std::cout << str;
-		}
-	}
-	std::cout << std::endl;
+	
 	return 0;
 }
--- essai/utility.h
+++ essai/utility.h
@@ -1,35 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   utility.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/15 15:17:48 by stgerard          #+#    #+#             */
-/*   Updated: 2023/01/16 17:30:53 by stgerard         ###   ########.fr       */
+/*   Created: 2023/01/16 14:31:45 by stgerard          #+#    #+#             */
+/*   Updated: 2023/01/16 15:16:40 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <string>
-#include <locale>
+#ifndef __UTILITY_H__
+#define __UTILITY_H__
 
-int main (int argc, char **argv)
+#include <cstring>
+
+template <typename T>
+inline const T & Mini(const T & a, const T & b)
 {
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else if (argc > 1)
-	{
-		for (size_t i = 1; argv[i]; i++)
-		{
-			std::string	str(argv[i]); 
-			for (size_t j = 0; j < str.length(); j++)
-			{
-				str[j] = std::toupper(str[j]);
-			}
-			std::cout << str;
-		}
-	}
-	std::cout << std::endl;
-	return 0;
+	return a < b ? a : b;
 }
+
+inline const char * Mini(const char * s1, const char * s2)
+{
+	return strcmp(s1, s2) < 0 ? s1 : s2;
+}
+
+#endif
