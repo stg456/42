af9debab7718a7e2d5f28840fa2f141abed386a9
stgerard <stgerard@student.42mulhouse.fr>
2023-01-16 12:19:50 +0100

    kjthgr

--- cpp/Module 00/ex00/megaphone.cpp
+++ cpp/Module 00/ex00/megaphone.cpp
@@ -6,17 +6,27 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 15:17:48 by stgerard          #+#    #+#             */
-/*   Updated: 2023/01/15 16:23:10 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/01/16 12:19:44 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-using namespace std;
+#include <string>
 
 int main ()
 {
-	cout << "BONJOUR" << endl << "AU REVOIR";
+	int age(0);
+	std::string nom("sans nom");
+	// nom.
+	std::cout << "Bonjour quel âge avez-vous?" << std::endl;
+	std::cin >> age;
+	std::cout << "Vous avez "<< age << " ans." << std::endl;
+
+	getline(std::cin, nom);
+
+	std::cout << "AU REVOIR" << std::endl;
 
 	return 0;
 }
+
+// essai
