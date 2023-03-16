cbaba958c5e78181e49ef5c5c36e0800f089a994
stgerard <stgerard@student.42mulhouse.fr>
2023-03-16 15:51:22 +0100

    deb

---
+++ cub3d/cub3d.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/16 15:44:48 by stgerard          #+#    #+#             */
+/*   Updated: 2023/03/16 15:50:56 by stgerard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <mlx.h>
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <string.h>
+# include <errno.h>
+# include <stdarg.h>
+# include <sys/types.h>
+# include "./libft/libft.h"
+
+# define T_S 63
+# define ESC 53
+# define W 13
+# define A 0
+# define S 1
+# define D 2
+
+
+
+#endif
