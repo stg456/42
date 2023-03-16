0f4d5366812583588a31e615aed5f6ae41b900ee
stgerard <stgerard@student.42mulhouse.fr>
2023-03-16 18:16:51 +0100

    yjht

--- cub3d/src/init.c
+++ cub3d/src/init.c
@@ -6,11 +6,44 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 17:08:08 by stgerard          #+#    #+#             */
-/*   Updated: 2023/03/16 17:50:38 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/03/16 18:16:41 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
+
+char	**map_read(char *filename)
+{
+	int		fd;
+	int		i;
+	char	*buf;
+	char	*tmp;
+	char	**arr;
+
+	i = 0;
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	buf = malloc(sizeof(char) * 1);
+	if (!buf)
+		return (NULL);
+	buf[0] = '\0';
+	tmp = buf;
+	while (42)
+	{
+		tmp = get_next_line(fd);
+		if (tmp == NULL)
+			break ;
+		buf = strjoin(buf, tmp);
+		if (buf == NULL)
+			return (NULL);
+		free(tmp);
+	}
+	arr = ft_split(buf, '\n');
+	free(buf);
+	close(fd);
+	return (arr);
+}
 
 char	**init_map(char **argv, t_env *e)
 {
