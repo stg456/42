6ede34d222206f4130861c344bfb6f26140df029
stgerard <stgerard@student.42mulhouse.fr>
2023-03-20 15:16:55 +0100

    kuyjfty

--- exam03/ft_printf.c
+++ exam03/ft_printf.c
@@ -1,55 +1,92 @@
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-void	ft_putstr2(char *str, int *len)
+int put_s(char *str)
 {
+	int i = 0;
 	if (!str)
-		str = "(NULL)";
-	while (*str)
-		*len += write(1, str++, 1);
+		str = "(null)";
+	while (str[i])
+		i += write(1, &str[i], 1);
+	return (i);
 }
 
-void	ft_putdigit(long long int nb, int base, int *len)
+int put_d(long long nb)
 {
-	char	*hex;
+	int i = 0;
+	char *s = "0123456789";
 
-	hex = "0123456789abcdef";
 	if (nb < 0)
 	{
-		nb *= -1;
-		*len += write(1, '-', 1);
+		write(1, "-", 1);
+		nb = -nb;
+		i++;
 	}
-	if (nb >= base)
+	if (nb >= 10)
 	{
-		ft_putdigit((nb / base), base, len);
+		i += put_d(nb / 10);
+		i += put_d(nb % 10);
 	}
-	*len += write(1, &hex[nb % base], 1);
+	if (nb < 10)
+	{
+		i += write(1, &s[nb % 10], 1);
+	}
+	return (i);
+}
+
+int put_x(unsigned int nb)
+{
+	int i = 0;
+	char *s = "0123456789abcdef";
+
+	if (nb >= 16)
+	{
+		i += put_x(nb / 16);
+		i += put_x(nb % 16);
+	}
+	if (nb < 10)
+	{
+		i += write(1, &s[nb % 16], 1);
+	}
+	return (i);
 }
 
 int	ft_printf(const char *format, ...)
 {
-	int len = 0;
-	va_list ptr;
-	
-	va_start (ptr, format);
+	int i = 0;
+	int res = 0;
+	va_list	ptr;
 
-	while (*format)
+	va_start(ptr, format);
+	while (format[i])
 	{
-		if (*format == '%' && *(format + 1))
+		if (format[i] == '%')
 		{
-			format++;
-			if (*format == 's')
-				ft_putstr();
-			else if (*format == 'd')
-				;
-			else if (*format == 'x')
-				;
+			i++;
+			if (format[i] == 's')
+				res += put_s(va_arg(ptr, char *));
+			else if (format[i] == 'd')
+				res += put_d(va_arg(ptr, long long));
+			else if (format[i] == 'x')
+				res += put_x(va_arg(ptr, unsigned int));
 		}
 		else
-			len += write(1, format, 1);
-		format++;
+			res += write(1, &format[i], 1);
+		i++;
 	}
-	return (va_end(ptr), len);
+	va_end(ptr);
+	return (res);
+}
+
+int main()
+{
+	char *str = NULL;
+	long long nb = -3372562354384384;
+	unsigned int hex = 10000;
+
+	printf("str: %s, int:%lld, hex:%x\n", str, nb, hex);
+	ft_printf("str: %s, int:%d, hex:%x\n", str, nb, hex);
+	return (0);
 }
