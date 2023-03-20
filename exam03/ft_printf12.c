95e7eda55e950c5379792a70336e173108cccd21
stgerard <stgerard@student.42mulhouse.fr>
2023-03-20 14:08:50 +0100

    ytr

--- exam03/ft_printf12.c
+++ exam03/ft_printf12.c
@@ -3,6 +3,53 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+int	put_s(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		str = "(null)";
+	while (str[i])
+		i += write(1, &str[i], 1);
+	return (i);
+}
+
+int	put_d(int nb)
+{
+	int i = 0;
+	char *s = "0123456789";
+	
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+		i++;
+	}
+	if (nb >= 10)
+	{
+		i += put_d(nb / 10);
+		i += put_d(nb % 10);
+	}
+	if (nb < 10)
+		i += write(1, &s[nb % 10], 1);
+	return (i);
+}
+
+int put_x(unsigned int nb)
+{
+	int i = 0;
+	char *s = "0123456789abcdef";
+
+	if (nb >= 16)
+	{
+		i += put_x(nb / 16);
+		i += put_x(nb % 16);
+	}
+	if (nb < 16)
+		i += write(1, &s[nb % 16], 1);
+	return (i);
+}
+
 int	ft_printf(const char *format, ...)
 {
 	int			i = 0;
@@ -16,14 +63,14 @@ int	ft_printf(const char *format, ...)
 		{
 			i++;
 			if (format[i] == 's')
-				res += puts(va_arg(ptr, char *));
-			else if (format[i] == 's')
-				putd(va_arg(ptr, int));
-			else if (format[i] == 's')
-				putx(va_arg(ptr, unsigned int));
+				res += put_s(va_arg(ptr, char *));
+			else if (format[i] == 'd')
+				res += put_d(va_arg(ptr, long long int));
+			else if (format[i] == 'x')
+				res += put_x(va_arg(ptr, unsigned int));
 		}
 		else
-			;
+			res += write(1, &format[i], 1);
 		i++;
 	}
 	va_end(ptr);
@@ -31,3 +78,14 @@ int	ft_printf(const char *format, ...)
 }
 
 // printf("\033[2K\r");
+
+int main()
+{
+	char *str = "";
+	int nb = -3372562;
+	unsigned int hex = 10000;
+
+	printf("str: %s, int:%d, hex:%x\n", str, nb, hex);
+	ft_printf("str: %s, int:%d, hex:%x\n", str, nb, hex);
+	return (0);
+}
