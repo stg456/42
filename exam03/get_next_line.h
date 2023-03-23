a772029eb6c680f08f15d262d56d50feee2421ca
stgerard <stgerard@student.42mulhouse.fr>
2023-03-23 14:21:48 +0100

    iuy

--- exam03/get_next_line.h
+++ exam03/get_next_line.h
@@ -1,13 +1,18 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+#endif
+
+// # include <unistd.h>
+// # include <stdlib.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
 
 # include <unistd.h>
 # include <stdlib.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-// # endif
+# endif
 
 char	*get_next_line(int fd);
 
-#endif
+// #endif
