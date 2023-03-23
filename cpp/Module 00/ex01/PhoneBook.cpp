da335f779396b0d0eaa9e9a171ebe873defea2e6
stgerard <stgerard@student.42mulhouse.fr>
2023-03-23 16:53:01 +0100

    okiy

--- cpp/Module 00/ex01/PhoneBook.cpp
+++ cpp/Module 00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 18:04:14 by stgerard          #+#    #+#             */
-/*   Updated: 2023/03/22 17:21:54 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/03/23 16:52:56 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,29 @@ int	main(void)
 {
 	std::string	cmd;
 
-	getline(std::cin, cmd);
-	if (std::cin >> ADD)
+	while (42)
 	{
-		std::cin >> FirsName;
-		std::cin >> LastName;
-		std::cin >> NickName;
-		std::cin >> PhoneNumber;
-		std::cin >> DartkestSecret;
-	}
-	else if (cmd == "SEARCH")
-	{
-		;
-	}
-	else if (cmd == "EXIT")
-	{
-		std::cin.clear();
-		return 0;
+		getline(std::cin, cmd);
+		if (cmd == "ADD")
+		{
+			getline(std::cin, FirstName);
+			std::cin >> Contact.FirsName;
+			std::cin >> LastName;
+			std::cin >> NickName;
+			std::cin >> PhoneNumber;
+			std::cin >> DartkestSecret;
+		}
+		else if (cmd == "SEARCH")
+		{
+			;
+		}
+		else if (cmd == "EXIT")
+		{
+			std::cin.clear();
+			return 0;
+		}
 	}
 	
-
-	// new contact
 
 	return 0;
 }
