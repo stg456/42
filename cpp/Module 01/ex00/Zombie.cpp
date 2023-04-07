b94e7a8efad898422f8a0d6fc5a0e059bb2ef9a5
stgerard <stgerard@student.42mulhouse.fr>
2023-04-07 19:42:39 +0200

    uyhtr

--- cpp/Module 01/ex00/Zombie.cpp
+++ cpp/Module 01/ex00/Zombie.cpp
@@ -6,9 +6,15 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 16:40:24 by stgerard          #+#    #+#             */
-/*   Updated: 2023/04/07 16:56:23 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/04/07 19:42:35 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+Zombie::zombie() {}
+
+void	zombie::announce(void)
+{
+
+}
