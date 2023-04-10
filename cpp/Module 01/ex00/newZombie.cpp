ab17bed9099c05b003d83a160151806c809b1709
stgerard <stgerard@student.42mulhouse.fr>
2023-04-10 14:44:05 +0200

    htgrf

--- cpp/Module 01/ex00/newZombie.cpp
+++ cpp/Module 01/ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 13:13:40 by stgerard          #+#    #+#             */
-/*   Updated: 2023/04/10 14:14:35 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/04/10 14:43:29 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 Zombie	*newZombie(std::string name)
 {
 	// Zombie *newzombie = new Zombie(name);
-	Zombie b("laurent");
 	
 	return (new Zombie(name));
 }
