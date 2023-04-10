b65263bf264501758e1bb14995af854d0eb81db9
stgerard <stgerard@student.42mulhouse.fr>
2023-04-10 16:41:14 +0200

    tgrfe

--- cpp/Module 01/ex01/zombieHorde.cpp
+++ cpp/Module 01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 14:53:46 by stgerard          #+#    #+#             */
-/*   Updated: 2023/04/10 15:32:53 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/04/10 16:41:05 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 Zombie* zombieHorde(int N, std::string name)
 {
-	Zombie *zombieHorde;
+	Zombie *zombie;
+
+	zombie = new Zombie[N];
 
 	for (int i = 0; i < N; i++)
 	{
 	
-		Zombie.announce();
+		zombie[i].announce();
 	}
-	Zombie *newzombie = new Zombie[N];
 
-	return (zombieHorde);
+	return (zombie);
 }
