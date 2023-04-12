92a68822ee337ccfd8ae8fd2bb17c77e72c952a1
stgerard <stgerard@student.42mulhouse.fr>
2023-04-12 19:20:37 +0200

    tgef

--- cpp/cpp01/ex03/HumanB.cpp
+++ cpp/cpp01/ex03/HumanB.cpp
@@ -6,7 +6,14 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 18:45:25 by stgerard          #+#    #+#             */
-/*   Updated: 2023/04/12 19:03:36 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/04/12 19:20:32 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+#include "Weapon.hpp"
+
+HumanB::HumanB() {}
+
+HumanB::~HumanB() {}
