f4c4a668cd012dded74eabdd1e0af3ba4d130463
stgerard <stgerard@student.42mulhouse.fr>
2023-04-13 16:46:29 +0200

    rbf

--- cpp/cpp01/ex03/Weapon.cpp
+++ cpp/cpp01/ex03/Weapon.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 18:45:43 by stgerard          #+#    #+#             */
-/*   Updated: 2023/04/13 16:31:44 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/04/13 16:46:25 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ Weapon::~Weapon() {}
 
 std::string Weapon::getType()
 {
-	return (_type);
+	return (this->_type);
 }
 
 void	setType(std::string type)
