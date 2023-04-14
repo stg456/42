24c5a4862ee559966b094a0d29a032c78e21a76f
stgerard <stgerard@student.42mulhouse.fr>
2023-04-14 16:01:41 +0200

    hgfe

--- cpp/cpp01/ex04/main.cpp
+++ cpp/cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 13:41:28 by stgerard          #+#    #+#             */
-/*   Updated: 2023/04/14 15:23:34 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/04/14 16:01:35 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,17 @@ int	main(int ac, char **av)
 {
 	std::string	str;
 	std::string	buf;
+	int 		i;
 	std::string	s1 = av[2];
 	std::string	s2 = av[3];
 	std::ifstream myfile (av[1]);
+
 	if (myfile.open())
 	{
 		
 	}
+	else
+		std::cout << "Impossible de lire le fichier !" << std::endl;
 
 
 
