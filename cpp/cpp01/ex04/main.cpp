ad0bc2b793e923c45212b5bab0c52c786328f106
stgerard <stgerard@student.42mulhouse.fr>
2023-04-14 14:39:46 +0200

    ujyhrt

--- cpp/cpp01/ex04/main.cpp
+++ cpp/cpp01/ex04/main.cpp
@@ -6,25 +6,27 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 13:41:28 by stgerard          #+#    #+#             */
-/*   Updated: 2023/04/14 13:57:45 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/04/14 14:39:42 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "ifstream.hpp"
-#include "ofstream.hpp"
 
 int	main(int ac, char **av)
 {
 	std::string	fichier;
-	std::string	s1;
-	std::string	s2;
+	std::string	s1 = "toto";
+	std::string	s2 = "titi";
 
-	ifstream f ("toto.txt");
-
-
+	std::ofstream f("fichier.txt");
+	if (!f.is_open())
+		std::cout << "Impossible d'ouvrir le fichier en écriture !" << std::endl;
+	else
+	{
+		;
+	}
+	f.close();
 	return 0;	
-
 }
