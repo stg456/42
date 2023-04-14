bd1d3f9ab829501c8778bd1c271e8f2b58b3fc90
stgerard <stgerard@student.42mulhouse.fr>
2023-04-14 17:13:27 +0200

    hhgs

--- cpp/cpp01/ex04/main.cpp
+++ cpp/cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 13:41:28 by stgerard          #+#    #+#             */
-/*   Updated: 2023/04/14 16:01:35 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/04/14 17:13:22 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,30 @@
 
 int	main(int ac, char **av)
 {
-	std::string	str;
-	std::string	buf;
-	int 		i;
-	std::string	s1 = av[2];
-	std::string	s2 = av[3];
-	std::ifstream myfile (av[1]);
+	std::string		str;
+	std::string		buf;
+	int 			i = 0;
+	int				j = 0;
+	std::string		s1 = av[2];
+	std::string		s2 = av[3];
+	std::ifstream	myfile (av[1]);
 
 	if (myfile.open())
 	{
-		
+		while (getline(myfile, buf))
+		{
+			if ()
+			{
+			
+			}
+		}
 	}
 	else
 		std::cout << "Impossible de lire le fichier !" << std::endl;
 
 
 
-	std::ofstream f("fichier.txt");
-	if (!f.is_open())
-		std::cout << "Impossible d'ouvrir le fichier en écriture !" << std::endl;
-	else
-	{
-		;
-	}
-	f.close();
+
+	myfile.close();
 	return 0;	
 }
