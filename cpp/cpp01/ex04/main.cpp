f77ff631034b06ccc8d7308aaafc3838bdee2386
stgerard <stgerard@student.42mulhouse.fr>
2023-04-15 17:46:56 +0200

    jhyt

--- cpp/cpp01/ex04/main.cpp
+++ cpp/cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 13:41:28 by stgerard          #+#    #+#             */
-/*   Updated: 2023/04/15 16:06:54 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/04/15 17:46:52 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,51 +16,54 @@
 
 int	main(int ac, char **av)
 {
-	std::string		str;
-	std::string		buf;
-	int 			i = 0;
-	int				j = 0;
+	std::string		str = (av[1]);
 	std::string		s1 = av[2];
 	std::string		s2 = av[3];
-	std::ifstream	myfile (av[1]);
-	std::string		resfile = (av[1]);
+	std::ifstream	myfile = str + ".replace";
+
+	std::cout << myfile.replace << std::endl;
 
 	if (ac != 4)
 	{
 		std::cout << "Erreur dans le nombre d'arguments !" << std::endl;
 		return 0;
 	}
-	if (s1.length == 0 || s2.length == 0)
-	{
-		std::cout << "Erreur dans les arguments à remplacer !" << std::endl;
-		return 0;
-	}
+
 	if (!myfile)
 	{
 		std::cout << "Erreur, il n'y a pas de fichier !" << std::endl;
 		return 0;
 	}
 
-
-
-	resfile.append(".replace");
-
-	if (myfile.open())
+	if (s1.empty() || s2.empty())
 	{
-		while (getline(myfile, buf))
-		{
-			if ()
-			{
-
-			}
-		}
+		std::cout << "Erreur dans les arguments à remplacer !" << std::endl;
+		return 0;
 	}
-	else
-		std::cout << "Impossible de lire le fichier !" << std::endl;
 
 
 
 
-	myfile.close();
+
+
+	
+	// if (myfile.open())
+	// {
+	// 	while (getline(myfile, buf))
+	// 	{
+	// 		if ()
+	// 		{
+
+	// 		}
+	// 	}
+	// }
+	// else
+	// 	std::cout << "Impossible de lire le fichier !" << std::endl;
+
+
+
+
+	// myfile.close();
+	
 	return 0;	
 }
