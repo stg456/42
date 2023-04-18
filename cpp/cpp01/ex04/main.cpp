638d7490ecf8dab2f6fadedce5735e98dd580c68
stgerard <stgerard@student.42mulhouse.fr>
2023-04-18 19:07:30 +0200

    rdtfyjgu

--- cpp/cpp01/ex04/main.cpp
+++ cpp/cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 13:41:28 by stgerard          #+#    #+#             */
-/*   Updated: 2023/04/18 17:08:08 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/04/18 19:06:59 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@
 
 int	main(int ac, char **av)
 {
-	char			str[100];
 	size_t			pos;
+	std::string		str;
 	std::string		s1 = av[2];
 	std::string		s2 = av[3];
 	std::string		filename = av[1];
-	std::ifstream	myfile (av[1], std::ios::binary | std::ios::out | std::ios::in);
+	std::ifstream	myfile (av[1], std::ios::binary | std::ios::in | std::ios::out);
 
-	// if (ac != 4)
-	// {
-	// 	std::cout << "Bad number of arguments !" << std::endl;
-	// 	return 1;
-	// }
+	if (ac != 4)
+	{
+		std::cout << "Bad number of arguments !" << std::endl;
+		return 1;
+	}
 
 	// if (s1.empty() || s2.empty())
 	// {
@@ -35,25 +35,36 @@ int	main(int ac, char **av)
 	// 	return 1;
 	// }
 
+	filename += ".replace"; //  creer le nom de fichier
+	std::cout << filename << std::endl;
 
-	// std::cout << filename << std::endl;
-	std::string filename2 = filename; //  creer le nom de fichier
-	filename2.append(".replace"); 
+	std::ofstream newfile(filename, std::ios::binary | std::ios::out | std::ios::in);
 
+	// if (newfile.is_open())
+	// {
+	// 	std::cout << "dans la boucle" << std::endl;
+	// 	while (getline(myfile, filename, '\0'))
+	// 	{
+	// 		newfile << filename;
+	// 		std::cout << filename << std::endl;
+	// 	}
+	// }
 
-	std::ofstream newfile(filename2, std::ios::binary | std::ios::out | std::ios::in);
-
-	if (newfile.is_open() && myfile.is_open())
+	// std::cout << myfile.is_open();
+	
+	if (myfile.is_open())
 	{
-		// fstream 
-		std::getline (myfile, filename2, '\0');
-		// std::cout << myfile << std::endl;
-		std::cout << filename << std::endl;
+		while (getline(myfile, str, '\0'))
+		{
+			// find, delete, insert
+			newfile << str << std::endl; // mets le contenu dans le nouveau fichier
+			std::cout << str << std::endl;
+			str.find(s1);
+			str.erase(s1);
+			str.insert(s2);
+		}
+		
 	}
-	else
-		std::cout << "pas dans la boucle" << std::endl;
-
-
 
 
 	
