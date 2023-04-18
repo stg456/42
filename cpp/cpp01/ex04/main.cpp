a0cdd4fc55886ab65eb1b056f371dc3e65c7ed30
stgerard <stgerard@student.42mulhouse.fr>
2023-04-18 16:39:07 +0200

    iuyu

--- cpp/cpp01/ex04/main.cpp
+++ cpp/cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 13:41:28 by stgerard          #+#    #+#             */
-/*   Updated: 2023/04/18 15:59:21 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/04/18 16:39:03 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ int	main(int ac, char **av)
 	std::string		filename = av[1];
 	std::ifstream	myfile (av[1]);
 
-	if (ac != 4)
-	{
-		std::cout << "Bad number of arguments !" << std::endl;
-		return 1;
-	}
+	// if (ac != 4)
+	// {
+	// 	std::cout << "Bad number of arguments !" << std::endl;
+	// 	return 1;
+	// }
 
 	// if (s1.empty() || s2.empty())
 	// {
@@ -38,20 +38,21 @@ int	main(int ac, char **av)
 	filename.append(".replace"); // creer le nom de fichier
 	std::cout << filename << std::endl;
 
-	std::ofstream newfile(filename, std::ios::binary);
-	while (str)
+	if (myfile.is_open())
 	{
-		filename << str;
+		std::getline (myfile, filename, '\0');
+		// std::cout << myfile << std::endl;
+		std::cout << filename << std::endl;
 	}
+	std::ofstream newfile(filename, std::ios::binary);
+	// while (str)
+	// {
+		// filename << str;
+	// }
 
 
-
-
-	// std::cout << str << std::endl;
 
 	
-
-
 	// myfile.close();
 	
 	return 0;	
