0462b9ffc45817500d759df65c00181a1193f90f
stgerard <stgerard@student.42mulhouse.fr>
2023-04-20 18:25:16 +0200

    hmgnf

--- cpp/cpp02/Fixed.hpp
+++ cpp/cpp02/Fixed.hpp
@@ -6,13 +6,31 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 17:59:06 by stgerard          #+#    #+#             */
-/*   Updated: 2023/04/20 18:02:59 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/04/20 18:25:12 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 ifndef FIXED_HPP
 #define FIXED_HPP
 
+# include <iostream>
 
+class	Fixed
+{
+		public:
+				// constructeur
+				Fixed(void);
+
+				~Fixed(void);
+
+				// fonction
+				int getRawBits(void) const;
+				void setRawBits(int const raw);
+
+		private:
+				int	nbFix;
+				static const int	bitFrac;
+
+};
 
 #endif
