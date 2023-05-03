b2a6fd308a7a483b8a38f441ebee6f3a74697fff
stgerard <stgerard@student.42mulhouse.fr>
2023-05-03 16:18:00 +0200

    yukkjt

--- cpp/cpp02/ex02/Fixed.cpp
+++ cpp/cpp02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 17:57:58 by stgerard          #+#    #+#             */
-/*   Updated: 2023/05/01 17:57:07 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/05/03 16:13:10 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,13 +80,13 @@ bool Fixed::operator==(const Fixed &rhs) const {return (_fix == rhs.getRawBits()
 
 bool Fixed::operator!=(const Fixed &rhs) const {return (_fix != rhs.getRawBits());}
 
-Fixed& Fixed::operator+(Fixed &rhs) const {return Fixed(toFloat() + rhs.toFloat());}
+float Fixed::operator+(Fixed rhs) const {return this->toFloat() + rhs.toFloat();}
 
-Fixed& Fixed::operator-(Fixed &rhs) const {return Fixed(toFloat() - rhs.toFloat());}
+float Fixed::operator-(Fixed rhs) const {return this->toFloat() - rhs.toFloat();}
 
-Fixed& Fixed::operator*(Fixed &rhs) const {return Fixed(toFloat() * rhs.toFloat());}
+float Fixed::operator*(Fixed rhs) const {return this->toFloat() * rhs.toFloat();}
 
-Fixed& Fixed::operator/(Fixed &rhs) const {return Fixed(toFloat() / rhs.toFloat());}
+float Fixed::operator/(Fixed rhs) const {return this->toFloat() / rhs.toFloat();}
 
 // prefix
 
