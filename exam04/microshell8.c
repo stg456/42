1736bf4d4bc0eb1248bb021e93b7a63bf5a65064
stgerard <stgerard@student.42mulhouse.fr>
2023-05-04 17:14:25 +0200

    yteh

---
+++ exam04/microshell8.c
@@ -0,0 +1,27 @@
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int ac, char **av, char **env)
+{
+	int i = 1;
+
+	if (ac == 1)
+		return 0;
+	av[ac] = 0;
+	while (av[i-1] && av[i])
+	{
+		av = av + i;
+		i = 0;
+		while (av[i] && strcmp(av[i], ";") != 0 && strcmp(av[i], "|") != 0)
+			i++;
+		if (!strcmp(*av, "cd"))
+			built(av);
+		else 
+			exec(av, i, env);
+		i++;
+	}
+
+}
