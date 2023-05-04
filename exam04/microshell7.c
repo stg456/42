c331183007fdb74a0a122d32760a04141159233a
stgerard <stgerard@student.42mulhouse.fr>
2023-05-04 16:23:41 +0200

    ktyjut

--- exam04/microshell7.c
+++ exam04/microshell7.c
@@ -15,7 +15,55 @@ int print(char *str)
 
 int built(char **av)
 {
-	
+	if (av[2] && strcmp(av[2], ";") != 0 && strcmp(av[2], "|") != 0)
+		return (print("error: cd: bad arguments\n"));
+	if (chdir(av[1]) == -1)
+		return (print("error: cd: cannot change directory to ") & print(av[1]) & print("\n"));
+	return 0;
+}
+
+int exe(char **av, int i, char **env)
+{
+	int fd[2];
+	int pid = 0;
+	int next = 0;
+	if (av[i] && strcmp(av[i], "|") == 0)
+		next = 1;
+	if (av[i] == *av)
+		return 0;
+	if (pipe(fd) == -1)
+		return (print("error fatal\n"));
+	if (pid == -1)
+		return (print("error fatal\n"));
+	else if (pid == 0)
+	{
+		close(fd[0]);
+		dup2(g_fd, 0);
+		av[i] = 0;
+		if (next)
+			dup2(fd[1], 1);
+		if (g_fd != 0)
+			close(g_fd);
+		close(fd[1]);
+		if (execve(*av, av, env) == -1)
+		{
+			print("error: cannot execute ");
+            print(*av);
+            print("\n");
+            exit (0);
+		}
+	}
+	else
+	{
+		close(fd[1]);
+		waitpid(pid, NULL, 0);
+		if (g_fd != 0)
+			close(g_fd);
+		if (next)
+			g_fd = dup(fd[0]);
+		close(fd[0]);
+	}
+	return 0;
 }
 
 int main(int ac, char **av, char **env)
@@ -28,7 +76,7 @@ int main(int ac, char **av, char **env)
 	{
 		av = av + i;
 		i = 0;
-		while (av[i] && strcmp(av[i], ";" != 0) && strcmp(av[i], "|" != 0))
+		while (av[i] && strcmp(av[i], ";") != 0 && strcmp(av[i], "|") != 0)
 			i++;
 		if (!strcmp(av[i], "cd"))
 			built(av);
@@ -37,3 +85,5 @@ int main(int ac, char **av, char **env)
 		i++;
 	}
 }
+
+// ./a.out cd ";" /usr/bin/ls ./a.out
