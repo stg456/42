46d4e54c7a08bda931bfe1967f30038beeb48b96
stgerard <stgerard@student.42mulhouse.fr>
2023-05-09 18:25:42 +0200

    76u6yt

--- cpp/cpp03/ex01/main.cpp
+++ cpp/cpp03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 13:54:24 by stgerard          #+#    #+#             */
-/*   Updated: 2023/05/09 16:50:05 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/05/09 18:10:25 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,21 @@
 
 int main(void) 
 {
-	ClapTrap	round("Bob");
+	ClapTrap	clapTrap("Bob");
+	ScavTrap	scavTrap("Jack");
 
-	round.attack("Joe");
-	round.takeDamage(3);
-	round.beRepaired(2);
-	round.attack("Bob");
-	round.takeDamage(3);
-	round.beRepaired(2);
+	clapTrap.attack("Joe");
+	clapTrap.takeDamage(3);
+	clapTrap.beRepaired(2);
+	clapTrap.attack("Jack");
+	clapTrap.takeDamage(3);
+	clapTrap.beRepaired(2);
+
+	scavTrap.attack("Jack");
+	scavTrap.takeDamage(3);
+	scavTrap.beRepaired(2);
+	scavTrap.guardGate();
+
 
 	return 0;
 }
