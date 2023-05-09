bc786120c975a23017fe19392e6543ee53ddddb7
stgerard <stgerard@student.42mulhouse.fr>
2023-05-09 15:36:03 +0200

    iyujtr

--- cpp/cpp03/ex01/ScavTrap.hpp
+++ cpp/cpp03/ex01/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 11:42:48 by stgerard          #+#    #+#             */
-/*   Updated: 2023/05/09 15:02:58 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/05/09 15:34:41 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ class	ScavTrap : public ClapTrap
 	public:
 	ScavTrap();
 	ScavTrap(std::string Name);
-	ScavTrap(const ClapTrap &copy);
+	ScavTrap(const ScavTrap &copy);
+	ScavTrap& operator = (const ScavTrap &rhs);
 	~ScavTrap();
 
 	void	attack(const std::string &target);
