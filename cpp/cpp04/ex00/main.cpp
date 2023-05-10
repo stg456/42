76edd0b7190cee04656bf8515717ae462faacb4d
stgerard <stgerard@student.42mulhouse.fr>
2023-05-10 18:03:48 +0200

    yhtrg

--- cpp/cpp04/ex00/main.cpp
+++ cpp/cpp04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 16:09:29 by stgerard          #+#    #+#             */
-/*   Updated: 2023/05/10 16:14:28 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/05/10 18:03:36 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 int main() 
 {
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
 
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+	...
+	
 	return 0;
 }
