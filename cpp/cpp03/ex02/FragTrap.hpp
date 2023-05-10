14259390f5fdc27e4fb9a8f1fcddec9e970659e2
stgerard <stgerard@student.42mulhouse.fr>
2023-05-10 15:28:53 +0200

    ujyh

--- cpp/cpp03/ex02/FragTrap.hpp
+++ cpp/cpp03/ex02/FragTrap.hpp
@@ -6,14 +6,14 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 18:34:18 by stgerard          #+#    #+#             */
-/*   Updated: 2023/05/10 14:52:37 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/05/10 15:27:33 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRAGTRAP_HPP
 # define FRAGTRAP_HPP
 
-#include <iostream>
+#include "ClapTrap.hpp"
 
 class FragTrap : public ClapTrap
 {
@@ -24,6 +24,7 @@ public:
 	FragTrap& operator = (const FragTrap &rhs);
 	~FragTrap();
 
+	void	attack(const std::string &target);
 	void	highFivesGuys(void);
 };
 
--- cpp/cpp03/ex02/main.cpp
+++ cpp/cpp03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 13:54:24 by stgerard          #+#    #+#             */
-/*   Updated: 2023/05/10 14:49:15 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/05/10 15:17:13 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int main(void)
 	clapTrap.takeDamage(3);
 	clapTrap.beRepaired(2);
 
-	FragTrap.attack("John");
+	FragTrap.attack("Bob");
 	FragTrap.takeDamage(3);
 	FragTrap.beRepaired(2);
 	FragTrap.highFivesGuys();
