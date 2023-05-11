b1b1e95472a20b70ed4fddde5a6f71dc1f9aca28
stgerard <stgerard@student.42mulhouse.fr>
2023-05-11 13:01:33 +0200

    jhyhr

--- exam04/microshell10.c
+++ exam04/microshell10.c
@@ -4,6 +4,26 @@
 #include <stdio.h>
 #include <string.h>
 
+int ft_error(char *str, char *av)
+{
+	while (*str)
+		write(2, str++, 1);
+	if (av)
+		while (*av)
+			write(2, av++, 1);
+	write(2, "\n", 1);
+	return 1;
+}
+
+int ft_exe(char **av, int i, int tmpfd, char **env)
+{
+	av[i] = NULL;
+	dup2(tmpfd, STDIN_FILENO);
+	close(tmpfd);
+	execve(av[0], av, env);
+	return (ft_error("error: cannot execute ", av[0]));
+}
+
 int main(int ac, char **av, char **env)
 {
 	int i = 0;
@@ -28,13 +48,35 @@ int main(int ac, char **av, char **env)
 		else if ((av[i] == NULL || strcmp(av[i], ";") == 0) && i != 0)
 		{
 			if (fork() == 0)
-			{}
+			{
+				if (ft_exe(av, i , tmpfd, env))
+					return 1;
+			}
 			else
-			{}
+			{
+				close(tmpfd);
+				while (waitpid(-1, NULL, WUNTRACED) != -1)
+					;
+				tmpfd = dup(STDIN_FILENO);
+			}
 		}
 		else if (strcmp(av[i], "|") == 0 && i != 0)
 		{
-
+			pipe(fd);
+			if (fork() == 0)
+			{
+				dup2(fd[1], STDOUT_FILENO);
+				close(fd[0]);
+				close(fd[1]);
+				if (ft_exe(av, i, tmpfd, env))
+					return 1;
+			}
+			else
+			{
+				close(fd[1]);
+				close(tmpfd);
+				tmpfd = fd[0];
+			}
 		}
 	}
 	close(tmpfd);
