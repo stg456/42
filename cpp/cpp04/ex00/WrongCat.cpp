7dc87279ff3f6a204934adb8eb91d7f310fb985c
stgerard <stgerard@student.42mulhouse.fr>
2023-05-12 10:50:46 +0200

    mjgf

---
+++ cpp/cpp04/ex00/WrongCat.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongCat.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/12 10:44:25 by stgerard          #+#    #+#             */
+/*   Updated: 2023/05/12 10:50:41 by stgerard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "WrongCat.hpp"
+
+WrongCat::WrongCat() {
+	std::cout << "WrongCat default constructor called" << std::endl;
+}
+
+WrongCat::WrongCat(std::string type) {
+	std::cout << "WrongCat constructor with type called" << std::endl;
+}
+
+WrongCat::WrongCat(const WrongCat &copy) {
+	std::cout << "WrongCat copy constructor called" << std::endl;
+	*this = copy;
+}
+
+WrongCat& WrongCat::operator = (const WrongCat &rhs) {
+	std::cout << "WrongCat assignement operator called" << std::endl;
+	if (this == &rhs)
+		return (*this);
+	return (*this);
+}
+
+WrongCat::~WrongCat() {std::cout << "WrongCat destructor called" << std::endl;}
