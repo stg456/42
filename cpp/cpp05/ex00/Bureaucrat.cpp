5aabc18734a3b8280478127a14841b599a7db942
stgerard <stgerard@student.42mulhouse.fr>
2023-05-26 16:54:08 +0200

    hrtg

--- cpp/cpp05/ex00/Bureaucrat.cpp
+++ cpp/cpp05/ex00/Bureaucrat.cpp
@@ -6,9 +6,12 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 16:09:54 by stgerard          #+#    #+#             */
-/*   Updated: 2023/05/26 16:20:51 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/05/26 16:54:02 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+Bureaucrat::Bureaucrat(std::string name) {}
+
+Bureaucrat::~Bureaucrat() {}
