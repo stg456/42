f2e0d1a0004fdce1b41253c1881d0787a976f247
stgerard <stgerard@student.42mulhouse.fr>
2023-06-09 11:52:36 +0200

    gfdssdb

--- miniRT/src/parse.c
+++ miniRT/src/parse.c
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 16:56:56 by stgerard          #+#    #+#             */
-/*   Updated: 2023/06/08 18:30:14 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/06/09 11:52:04 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,34 @@ t_data	amb(char *buf, t_data d)
 {
 	t_amb	A;
 	char	*tmp;
-	int		i;
+	// int		i;
 
-	i = 0;
+	// i = 0;
+	tmp = NULL;
 	if (*buf != '\n')
 	{
-		printf("dans A; %s\n", buf);
-		
+		printf("dans A: %s\n", buf);		
 		if (ft_strncmp(buf, "A", 1) == 0)
 			d.nbA += 1;
-		
+		printf("%d\n", d.nbA);
 		while (*buf == ' ' || *buf == '\t')
 			buf++;
+		// if (*buf != ' ' || *buf != '\t')
+			if (*buf != ' ')
+				tmp = buf;
+		printf("tmp: %s\n", tmp);
 		A.ratioA = ft_atof(buf);
-		printf("%lf\n", A.ratioA);
-		while (*buf == ' ' || *buf == '\t')
-			buf++;
-		A.rgb.r = ft_atoi(buf);
-		printf("%d\n", A.rgb.r);
-		buf++;
-		A.rgb.g = ft_atoi(buf);
-		buf++;
-		A.rgb.b = ft_atoi(buf);
+		printf("ratio: %lf\n", A.ratioA);
+		// while (*buf == ' ' || *buf == '\t')
+		// 	buf++;
+		// while (*buf != ',')
+		// 	A.rgb.r = ft_atoi(buf);
+		// printf("%d\n", A.rgb.r);
+
+		// buf++;
+		// A.rgb.g = ft_atoi(buf);
+		// buf++;
+		// A.rgb.b = ft_atoi(buf);
 	}
 	return (d);
 }
