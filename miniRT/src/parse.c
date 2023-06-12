f85234376e932d1c8a65c9be1bb91995219d64fa
stgerard <stgerard@student.42mulhouse.fr>
2023-06-12 16:10:52 +0200

    parse

--- miniRT/src/parse.c
+++ miniRT/src/parse.c
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 16:56:56 by stgerard          #+#    #+#             */
-/*   Updated: 2023/06/12 15:47:18 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/06/12 16:09:07 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	amb(char *buf, t_data d)
 	char	**tmp;
 	char	**tmpcolor;
 
-	tmp = malloc(sizeof(char *) *	ft_strlen(buf) + 1);
+	tmp = malloc(sizeof(char *) * ft_strlen(buf) + 1);
 	pass(buf);
 	printf("dans A: %s\n", buf);
 	tmp = ft_split(buf, ' ');
@@ -28,7 +28,6 @@ int	amb(char *buf, t_data d)
 	A.rgb.r = ft_atoi(tmpcolor[0]);
 	A.rgb.g = ft_atoi(tmpcolor[1]);
 	A.rgb.b = ft_atoi(tmpcolor[2]);
-
 	free(tmp);
 	return (d.nbA);
 }
@@ -40,7 +39,7 @@ int	cam(char *buf, t_data d)
 	char	**tmp_pos;
 	char	**tmp_axe;
 
-	tmp = malloc(sizeof(char *) *	ft_strlen(buf) + 1);
+	tmp = malloc(sizeof(char *) * ft_strlen(buf) + 1);
 	pass(buf);
 	printf("dans C: %s\n", buf);
 	tmp = ft_split(buf, ' ');
@@ -54,7 +53,6 @@ int	cam(char *buf, t_data d)
 	C.axe.y = ft_atof(tmp_axe[1]);
 	C.axe.z = ft_atof(tmp_axe[2]);
 	C.fov = ft_atoi(tmp[3]);
-	// printf("fov: %d\n", C.fov);
 	free(tmp);
 	return (d.nbC);
 }
@@ -66,7 +64,7 @@ int	lum(char *buf, t_data d)
 	char	**tmp_pos;
 	char	**tmpcolor;
 
-	tmp = malloc(sizeof(char *) *	ft_strlen(buf) + 1);
+	tmp = malloc(sizeof(char *) * ft_strlen(buf) + 1);
 	pass(buf);
 	printf("dans L: %s\n", buf);
 	tmp = ft_split(buf, ' ');
@@ -80,8 +78,6 @@ int	lum(char *buf, t_data d)
 	L.rgb.r = ft_atoi(tmpcolor[0]);
 	L.rgb.g = ft_atoi(tmpcolor[1]);
 	L.rgb.b = ft_atoi(tmpcolor[2]);
-	printf("ratio: %lf\n", L.ratioL);
-
 	free(tmp);
 	return (d.nbL);
 }
@@ -93,7 +89,7 @@ int	sp(char *buf, t_data d)
 	char	**tmp_pos;
 	char	**tmpcolor;
 
-	tmp = malloc(sizeof(char *) *	ft_strlen(buf) + 1);
+	tmp = malloc(sizeof(char *) * ft_strlen(buf) + 1);
 	pass(buf);
 	printf("dans sp: %s\n", buf);
 	tmp = ft_split(buf, ' ');
@@ -107,19 +103,66 @@ int	sp(char *buf, t_data d)
 	sp.rgb.r = ft_atoi(tmpcolor[0]);
 	sp.rgb.g = ft_atoi(tmpcolor[1]);
 	sp.rgb.b = ft_atoi(tmpcolor[2]);
-	printf("diam: %lf\n", sp.diam);
-
 	free(tmp);
 	return (d.nbsp);
 }
 
 int	pl(char *buf, t_data d)
 {
+	t_pl	pl;
+	char	**tmp;
+	char	**tmp_pos;
+	char	**tmp_axe;
+	char	**tmpcolor;
 
+	tmp = malloc(sizeof(char *) * ft_strlen(buf) + 1);
+	pass(buf);
+	printf("dans pl: %s\n", buf);
+	tmp = ft_split(buf, ' ');
+	d.nbpl += 1;
+	tmp_pos = ft_split(tmp[1], ',');
+	pl.pos.x = ft_atof(tmp_pos[0]);
+	pl.pos.y = ft_atof(tmp_pos[1]);
+	pl.pos.z = ft_atof(tmp_pos[2]);
+	tmp_axe = ft_split(tmp[2], ',');
+	pl.axe.x = ft_atof(tmp_axe[0]);
+	pl.axe.y = ft_atof(tmp_axe[1]);
+	pl.axe.z = ft_atof(tmp_axe[2]);
+	tmpcolor = ft_split(tmp[3], ',');
+	pl.rgb.r = ft_atoi(tmpcolor[0]);
+	pl.rgb.g = ft_atoi(tmpcolor[1]);
+	pl.rgb.b = ft_atoi(tmpcolor[2]);
+	free(tmp);
+	return (d.nbpl);
 }
 
 int	cyl(char *buf, t_data d)
 {
+	t_cy	cy;
+	char	**tmp;
+	char	**tmp_pos;
+	char	**tmp_axe;
+	char	**tmpcolor;
 
+	tmp = malloc(sizeof(char *) * ft_strlen(buf) + 1);
+	pass(buf);
+	printf("dans cy: %s\n", buf);
+	tmp = ft_split(buf, ' ');
+	d.nbcy += 1;
+	tmp_pos = ft_split(tmp[1], ',');
+	cy.pos.x = ft_atof(tmp_pos[0]);
+	cy.pos.y = ft_atof(tmp_pos[1]);
+	cy.pos.z = ft_atof(tmp_pos[2]);
+	tmp_axe = ft_split(tmp[2], ',');
+	cy.axe.x = ft_atof(tmp_axe[0]);
+	cy.axe.y = ft_atof(tmp_axe[1]);
+	cy.axe.z = ft_atof(tmp_axe[2]);
+	cy.diam = ft_atof(tmp[3]);
+	cy.height = ft_atof(tmp[4]);
+	tmpcolor = ft_split(tmp[5], ',');
+	cy.rgb.r = ft_atoi(tmpcolor[0]);
+	cy.rgb.g = ft_atoi(tmpcolor[1]);
+	cy.rgb.b = ft_atoi(tmpcolor[2]);
+	free(tmp);
+	return (d.nbcy);
 }
-
