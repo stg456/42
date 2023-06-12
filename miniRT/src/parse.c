f661f07559efab49fa06bf8ef22469dc41cf69b7
stgerard <stgerard@student.42mulhouse.fr>
2023-06-12 16:15:44 +0200

    ythrgr

--- miniRT/src/parse.c
+++ miniRT/src/parse.c
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 16:56:56 by stgerard          #+#    #+#             */
-/*   Updated: 2023/06/12 16:09:07 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/06/12 16:15:02 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	amb(char *buf, t_data d)
 {
-	t_amb	A;
+	t_amb	a;
 	char	**tmp;
 	char	**tmpcolor;
 
@@ -23,18 +23,18 @@ int	amb(char *buf, t_data d)
 	printf("dans A: %s\n", buf);
 	tmp = ft_split(buf, ' ');
 	d.nbA += 1;
-	A.ratioA = ft_atof(tmp[1]);
+	a.ratioA = ft_atof(tmp[1]);
 	tmpcolor = ft_split(tmp[2], ',');
-	A.rgb.r = ft_atoi(tmpcolor[0]);
-	A.rgb.g = ft_atoi(tmpcolor[1]);
-	A.rgb.b = ft_atoi(tmpcolor[2]);
+	a.rgb.r = ft_atoi(tmpcolor[0]);
+	a.rgb.g = ft_atoi(tmpcolor[1]);
+	a.rgb.b = ft_atoi(tmpcolor[2]);
 	free(tmp);
 	return (d.nbA);
 }
 
 int	cam(char *buf, t_data d)
 {
-	t_cam	C;
+	t_cam	c;
 	char	**tmp;
 	char	**tmp_pos;
 	char	**tmp_axe;
@@ -45,21 +45,21 @@ int	cam(char *buf, t_data d)
 	tmp = ft_split(buf, ' ');
 	d.nbC += 1;
 	tmp_pos = ft_split(tmp[1], ',');
-	C.pos.x = ft_atof(tmp_pos[0]);
-	C.pos.y = ft_atof(tmp_pos[1]);
-	C.pos.z = ft_atof(tmp_pos[2]);
+	c.pos.x = ft_atof(tmp_pos[0]);
+	c.pos.y = ft_atof(tmp_pos[1]);
+	c.pos.z = ft_atof(tmp_pos[2]);
 	tmp_axe = ft_split(tmp[2], ',');
-	C.axe.x = ft_atof(tmp_axe[0]);
-	C.axe.y = ft_atof(tmp_axe[1]);
-	C.axe.z = ft_atof(tmp_axe[2]);
-	C.fov = ft_atoi(tmp[3]);
+	c.axe.x = ft_atof(tmp_axe[0]);
+	c.axe.y = ft_atof(tmp_axe[1]);
+	c.axe.z = ft_atof(tmp_axe[2]);
+	c.fov = ft_atoi(tmp[3]);
 	free(tmp);
 	return (d.nbC);
 }
 
 int	lum(char *buf, t_data d)
 {
-	t_lum	L;
+	t_lum	l;
 	char	**tmp;
 	char	**tmp_pos;
 	char	**tmpcolor;
@@ -70,14 +70,14 @@ int	lum(char *buf, t_data d)
 	tmp = ft_split(buf, ' ');
 	d.nbC += 1;
 	tmp_pos = ft_split(tmp[1], ',');
-	L.pos.x = ft_atof(tmp_pos[0]);
-	L.pos.y = ft_atof(tmp_pos[1]);
-	L.pos.z = ft_atof(tmp_pos[2]);
-	L.ratioL = ft_atof(tmp[2]);
+	l.pos.x = ft_atof(tmp_pos[0]);
+	l.pos.y = ft_atof(tmp_pos[1]);
+	l.pos.z = ft_atof(tmp_pos[2]);
+	l.ratioL = ft_atof(tmp[2]);
 	tmpcolor = ft_split(tmp[3], ',');
-	L.rgb.r = ft_atoi(tmpcolor[0]);
-	L.rgb.g = ft_atoi(tmpcolor[1]);
-	L.rgb.b = ft_atoi(tmpcolor[2]);
+	l.rgb.r = ft_atoi(tmpcolor[0]);
+	l.rgb.g = ft_atoi(tmpcolor[1]);
+	l.rgb.b = ft_atoi(tmpcolor[2]);
 	free(tmp);
 	return (d.nbL);
 }
