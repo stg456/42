e05a664978f02216eaeedff6740df858f8355cb1
stgerard <stgerard@student.42mulhouse.fr>
2023-06-13 14:27:31 +0200

    oiuy

--- miniRT/src/check2.c
+++ miniRT/src/check2.c
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 11:24:18 by stgerard          #+#    #+#             */
-/*   Updated: 2023/06/13 14:17:33 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/06/13 14:25:29 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ void	check_amb(t_amb a)
 	if (rgbinrange(a.rgb.r) || rgbinrange(a.rgb.g) || rgbinrange(a.rgb.b))
 		ft_error("Error\nincorrect rgb value for A\n");
 	printf("check2b a.rgb.b: %d\n", a.rgb.b);
+	printf("rgbinrange(a.rgb.b): %d\n", rgbinrange(a.rgb.b));
 	// printf("check2b l.rgb.b: %d\n", l.rgb.b);
 }
