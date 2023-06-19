c9418f3fadf2290a0a5309291927c05984dada87
stgerard <stgerard@student.42mulhouse.fr>
2023-06-19 17:40:48 +0200

    rfrwf

--- cpp/cpp05/ex00/Bureaucrat.cpp
+++ cpp/cpp05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 16:09:54 by stgerard          #+#    #+#             */
-/*   Updated: 2023/05/26 16:54:02 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/06/19 17:40:05 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,17 @@
 Bureaucrat::Bureaucrat(std::string name) {}
 
 Bureaucrat::~Bureaucrat() {}
+
+Bureaucrat::GradeTooHighException() {}
+
+Bureaucrat::GradeTooLowException() {}
+
+Bureaucrat::getName() {}
+
+Bureaucrat::getGrade() {}
+
+Bureaucrat & oprator + () {}
+
+Bureaucrat & oprator - () {}
+
+Bureaucrat & oprator << () {}
