133497a7dd45cdd4320685c2f2b2049ea70fad2c
stgerard <stgerard@student.42mulhouse.fr>
2023-08-13 16:51:58 +0200

    tgr

--- cpp/cpp05/ex01/main.cpp
+++ cpp/cpp05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 16:11:17 by stgerard          #+#    #+#             */
-/*   Updated: 2023/08/13 16:36:52 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/08/13 16:49:36 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,11 @@ void test1() {
 	try 
 	{
 		Bureaucrat B1("Jim", 10);
-		Bureaucrat B2("Joe", 20);
 		Form F1("formulaire 1", 4, 10);
-		Form F2("formulaire 2", 20, 100);
 		std::cout << B1;
-		std::cout << B2;
 		std::cout << F1;
-		std::cout << F2;
 
 		B1.signForm(F1);
-		B1.signForm(F2);
-
 
 	}
 	catch (std::exception &e) 
@@ -38,18 +32,11 @@ void test1() {
 void test2() {
 	try 
 	{
-		Bureaucrat B1("Jim", 10);
 		Bureaucrat B2("Joe", 20);
-		Form F1("formulaire 1", 4, 10);
 		Form F2("formulaire 2", 20, 100);
-		std::cout << B1;
 		std::cout << B2;
-		std::cout << F1;
 		std::cout << F2;
 
-		B1.signForm(F1);
-		B2.signForm(F1);
-		B1.signForm(F2);
 		B2.signForm(F2);
 
 	}
@@ -59,28 +46,28 @@ void test2() {
 	}
 }
 
-void test3() {
-	try 
-	{
-		Bureaucrat B1("Jim", 10);
-		Bureaucrat B2("Joe", 20);
-		Form F1("formulaire 1", 4, 10);
-		Form F2("formulaire 2", 20, 10);
-		std::cout << B1;
-		std::cout << B2;
-		std::cout << F1;
-		std::cout << F2;
+// void test3() {
+// 	try 
+// 	{
+// 		Bureaucrat B1("Jim", 10);
+// 		Bureaucrat B2("Joe", 20);
+// 		Form F1("formulaire 1", 4, 10);
+// 		Form F2("formulaire 2", 20, 10);
+// 		std::cout << B1;
+// 		std::cout << B2;
+// 		std::cout << F1;
+// 		std::cout << F2;
 
-		B1.signForm(F1);
-		B1.signForm(F2);
+// 		B1.signForm(F1);
+// 		B1.signForm(F2);
 
 
-	}
-	catch (std::exception &e) 
-	{
-		std::cerr << e.what() << std::endl;
-	}
-}
+// 	}
+// 	catch (std::exception &e) 
+// 	{
+// 		std::cerr << e.what() << std::endl;
+// 	}
+// }
 
 int main() 
 {
@@ -88,8 +75,8 @@ int main()
 	test1();
 	std::cout << "___test2___" << std::endl;
 	test2();
-	std::cout << "___test3___" << std::endl;
-	test3();
+	// std::cout << "___test3___" << std::endl;
+	// test3();
 	
 	return 0;
 }
