30766cd612a142fe764d256b658de54895baf79f
stgerard <stgerard@student.42mulhouse.fr>
2023-09-08 13:18:08 +0200

    thgr

--- cpp/cpp05/ex02/PresidentialPardonForm.hpp
+++ cpp/cpp05/ex02/PresidentialPardonForm.hpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 11:05:43 by stgerard          #+#    #+#             */
-/*   Updated: 2023/09/08 13:03:27 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/09/08 13:18:00 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 class Bureaucrat;
 class Form;
 
-class PresidentialPardonForm
+class PresidentialPardonForm : public Form
 {
 	private:
 		std::string	_target;
