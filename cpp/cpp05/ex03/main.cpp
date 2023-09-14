1a721d164666c5ee23be6371b1cbeb7e316d89a5
stgerard <stgerard@student.42mulhouse.fr>
2023-09-14 16:00:26 +0200

    tr

--- cpp/cpp05/ex03/main.cpp
+++ cpp/cpp05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 16:11:17 by stgerard          #+#    #+#             */
-/*   Updated: 2023/09/14 15:41:21 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/09/14 15:59:18 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,40 +15,13 @@
 #include "ShrubberyCreationForm.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "Intern.hpp"
 
 void test() {
 
-        ShrubberyCreationForm	shrub("formulaire de création de Shrubbery");
-		RobotomyRequestForm		robot("formulaire de robotomysation");
-		PresidentialPardonForm	presid("formulaire de pardon présidentiel");
-        Bureaucrat B1("Jim", 130);	// 145 to sign, 137 to execute
-		Bureaucrat B2("Jess", 40);	// 72 to sign, 45 to execute
-		Bureaucrat B3("Jack", 5);	// 25 to sign, 5 to execute
-
-    try {
-        B1.signForm(shrub);
-		B1.executeForm(shrub);
-    }
-    catch(std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-	std::cout << std::endl;
-    try {
-        B2.signForm(robot);
-		B2.executeForm(robot);
-    }
-    catch(std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-	std::cout << std::endl;
-    try {
-        B3.signForm(presid);
-		B3.executeForm(presid);
-    }
-    catch(std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-	std::cout << std::endl;
+	Intern someRandomIntern;
+	Form* rrf;
+	rrf = someRandomIntern.makeForm("robotomy request", "Bender");
 }
 
 int main() 
@@ -56,5 +29,6 @@ int main()
 	std::cout << "___test___" << std::endl;
 	test();
 
+	delete[] new Intern;
 	return 0;
 }
