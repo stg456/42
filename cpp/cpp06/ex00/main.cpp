96b6008b878d818f5475a14f67f5a34c312dddb3
stgerard <stgerard@student.42mulhouse.fr>
2023-09-21 16:18:10 +0200

    ujyhtgr

--- cpp/cpp06/ex00/main.cpp
+++ cpp/cpp06/ex00/main.cpp
@@ -6,26 +6,34 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 09:49:57 by stgerard          #+#    #+#             */
-/*   Updated: 2023/09/19 10:47:08 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/09/21 16:17:01 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "ScalarConverter.hpp"
 
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
 	ScalarConverter	sc;
 	double			d = 0;
 
-	if (ac != 2) {
+	if (argc != 2) {
 		std::cout << "Wrong number of arguments" << std::endl;
 		return 1;
 	}
-	if (ac == 2) {
-		if (strlen(av[1]) == 1 && !isdigit(av[1][0]))
-			d = static_cast<double>(av[1][0]);
+	if (argc == 2) {
+	// 	if (sc.check(argv[1])) {
+	// 	std::cout << "char: impossible" << std::endl;
+	// 	std::cout << "int: impossible" << std::endl;
+	// 	std::cout << "float: nanf" << std::endl;
+	// 	std::cout << "double: nan" << std::endl;
+	// 	return 0;
+	// 	}
+		if (strlen(argv[1]) == 1 && !isdigit(argv[1][0]))
+			d = static_cast<double>(argv[1][0]);
+		
 		else {
-			d = std::strtod(av[1], NULL); // interprete comme un double, passe les whitspaces etc
+			d = std::strtod(argv[1], NULL); // interprete comme un double, passe les whitspaces etc
 			std::cout << "d: " << d << std::endl;
 		}
 	}
