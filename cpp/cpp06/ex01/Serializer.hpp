8e815710e11f9c9582b15887c7f6aa8c9a349694
stgerard <stgerard@student.42mulhouse.fr>
2023-09-22 15:39:41 +0200

    juyht

--- cpp/cpp06/ex01/Serializer.hpp
+++ cpp/cpp06/ex01/Serializer.hpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 13:47:03 by stgerard          #+#    #+#             */
-/*   Updated: 2023/09/22 15:23:12 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/09/22 15:39:28 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 struct Data {
 	std::string s1;
-	int n;
+	int	n;
 	std::string s2;
 };
 
