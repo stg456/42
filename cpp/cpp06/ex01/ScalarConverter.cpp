2de367d418f9c978c52ae27821f503ff7d19ca5d
stgerard <stgerard@student.42mulhouse.fr>
2023-09-22 11:42:39 +0200

    hygtr

---
+++ cpp/cpp06/ex01/ScalarConverter.cpp
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 09:45:26 by stgerard          #+#    #+#             */
+/*   Updated: 2023/09/22 11:33:56 by stgerard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::ScalarConverter(const ScalarConverter & copy) {
+	*this = copy;
+}
+
+ScalarConverter & ScalarConverter::operator = (const ScalarConverter & rhs) {
+	if (this == &rhs)
+		return *this;
+	return *this;
+}
+
+ScalarConverter::~ScalarConverter() {}
+
+
+int ScalarConverter::check(char *argv)
+{
+	if (strcmp(argv, "-inff") == 0 || strcmp(argv, "+inff") == 0 || strcmp(argv, "nanf") == 0 
+		|| strcmp(argv, "-inf") == 0 || strcmp(argv, "+inf") == 0 || strcmp(argv, "nan") == 0
+			|| strcmp(argv, "inf") == 0 || strcmp(argv, "inff") == 0)
+		return (0);
+	return (1);	
+}
+
+void	ScalarConverter::toChar(double d) {
+	if (d >= 32 && d <= 126)
+		std::cout << "char: " << static_cast<char>(d) << std::endl;
+	else
+		std::cout << "char: Non displayable" << std::endl;
+}
+
+void	ScalarConverter::toInt(double d) {
+	if (d > std::numeric_limits<int>::min() && d < std::numeric_limits<int>::max())
+		std::cout << "int: " << static_cast<int>(d) << std::endl;
+	else
+		std::cout << "int: impossible" << std::endl;
+}
+
+void ScalarConverter::toFloat(double d) {
+    if (std::abs(d) > std::numeric_limits<float>::min() && std::abs(d) < std::numeric_limits<float>::max()) {
+        float   f = static_cast<float>(d);
+        int     i = static_cast<int>(d);
+
+        std::cout << "float: " << static_cast<float>(d);
+        if (f - i == 0)
+            std::cout << ".0f" << std::endl;
+        else
+            std::cout << "f" << std::endl;
+    }
+    else
+        std::cout << "float: out of range" << std::endl;
+}
+
+void	ScalarConverter::toDouble(double d) {
+	if (std::abs(d) > std::numeric_limits<double>::min() && std::abs(d) < std::numeric_limits<double>::max()) {
+		int     i = static_cast<int>(d);
+		std::cout << "double: " << static_cast<double>(d);
+		if (d - i == 0)
+			std::cout << ".0" << std::endl;
+		else
+			std::cout << std::endl;
+	}
+	else
+		std::cout << "double: out of range" << std::endl;
+}
+
+void	ScalarConverter::convert(double d) {
+	ScalarConverter	sc;
+
+	sc.toChar(d);
+	sc.toInt(d);
+	sc.toFloat(d);
+	sc.toDouble(d);
+}
---
+++ cpp/cpp06/ex01/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 09:49:57 by stgerard          #+#    #+#             */
+/*   Updated: 2023/09/22 11:36:19 by stgerard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "ScalarConverter.hpp"
+
+int main(int argc, char **argv)
+{
+	ScalarConverter	sc;
+	double			d = 0;
+
+	if (argc != 2) {
+		std::cout << "Wrong number of arguments" << std::endl;
+		return 1;
+	}
+	if (argc == 2) {
+		if (!sc.check(argv[1])) {
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: nanf" << std::endl;
+		std::cout << "double: nan" << std::endl;
+		return 0;
+		}
+		if (strlen(argv[1]) == 1 && !isdigit(argv[1][0]))
+			d = static_cast<double>(argv[1][0]);
+		
+		else {
+			d = std::strtod(argv[1], NULL); // interprete comme un double, passe les whitspaces etc
+			// std::cout << "d: " << d << std::endl;
+		}
+	}
+	sc.convert(d);
+	
+	return 0;
+}
