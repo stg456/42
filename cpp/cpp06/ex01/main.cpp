21966b9021ef2d62a03347bfa1c715640710f5ed
stgerard <stgerard@student.42mulhouse.fr>
2023-09-22 16:03:36 +0200

    ex01

--- cpp/cpp06/ex01/main.cpp
+++ cpp/cpp06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 09:49:57 by stgerard          #+#    #+#             */
-/*   Updated: 2023/09/22 15:25:23 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/09/22 16:03:01 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,23 @@
 
 int main()
 {
+	Data* data = new Data;
 
-	
+	data->s1 = "Hello";
+	data->n = 42;
+	data->s2 = "World";
+
+	std::cout << "Data: " << data->s1 << " " << data->n << " " << data->s2 << std::endl;
+
+	uintptr_t raw = serialize(data);
+
+	std::cout << "Raw: " << raw << std::endl;
+
+	Data* data2 = deserialize(raw);
+
+	std::cout << "Data: " << data2->s1 << " " << data2->n << " " << data2->s2 << std::endl;
+
+	delete data;
+
 	return 0;
 }
