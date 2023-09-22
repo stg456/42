814ad296fc9916fa1d89ec0475dffff4aa2755d7
stgerard <stgerard@student.42mulhouse.fr>
2023-09-22 14:08:51 +0200

    yhgtrf

--- cpp/cpp06/ex01/Serializer.cpp
+++ cpp/cpp06/ex01/Serializer.cpp
@@ -1,21 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Serializer.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/18 09:49:57 by stgerard          #+#    #+#             */
+/*   Created: 2023/09/22 13:40:36 by stgerard          #+#    #+#             */
 /*   Updated: 2023/09/22 14:08:01 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Serializer.hpp"
-#include <iostream>
+# include "Serializer.hpp"
 
-int main(int argc, char **argv)
-{
+Serializer::Serializer() {}
 
-	
-	return 0;
+Serializer::Serializer(const Serializer& copy) {
+	*this = copy;
+}
+
+Serializer::~Serializer() {}
+
+Serializer& Serializer::operator = (const Serializer &rhs) {
+	if (this == &rhs)
+		return *this;
+	return *this;
+}
+
+
+uintptr_t Serializer::serialize(Data* ptr) {
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data* Serializer::deserialize(uintptr_t raw) {
+	return reinterpret_cast<Data*>(raw);
 }
--- cpp/cpp06/ex01/Serializer.hpp
+++ cpp/cpp06/ex01/Serializer.hpp
@@ -1,21 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/18 09:49:57 by stgerard          #+#    #+#             */
+/*   Created: 2023/09/22 13:47:03 by stgerard          #+#    #+#             */
 /*   Updated: 2023/09/22 14:08:01 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Serializer.hpp"
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
+
 #include <iostream>
 
-int main(int argc, char **argv)
+class Serializer
 {
+	public:
+		Serializer();
+		Serializer(const Serializer& copy);
+		~Serializer();
+		Serializer& operator = (const Serializer &src);
 
-	
-	return 0;
-}
+		uintptr_t	serialize(Data* ptr);
+		Data*		deserialize(uintptr_t raw);
+};
+
+#endif
