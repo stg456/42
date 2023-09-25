7604977d2c6bc0052f4cf8024fa2d9b848c06c62
stgerard <stgerard@student.42mulhouse.fr>
2023-09-25 16:00:54 +0200

    i786

--- cpp/cpp06/ex02/main.cpp
+++ cpp/cpp06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 09:49:57 by stgerard          #+#    #+#             */
-/*   Updated: 2023/09/25 15:32:33 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/09/25 16:00:49 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main()
 
 	Base* base = generate();
 
-
+	delete[] base;
 
 	return 0;
 }
