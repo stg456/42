4aa0a601e31d6dea607c6b3b8d904c85b6c8bffa
stgerard <stgerard@student.42mulhouse.fr>
2023-09-25 15:57:23 +0200

    oikuy

--- cpp/cpp06/ex02/Identify.cpp
+++ cpp/cpp06/ex02/Identify.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 16:44:15 by stgerard          #+#    #+#             */
-/*   Updated: 2023/09/25 15:32:33 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/09/25 15:57:16 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,19 @@ Base* Base::generate(void) {
 }
 
 void identify_from_pointer(Base* p) {
-
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
 }
 
 void identify_from_reference(Base & p) {
-
+	if (dynamic_cast<A*>(&p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(&p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(&p))
+		std::cout << "C" << std::endl;
 }
---
+++ cpp/cpp07/ex00/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 09:49:57 by stgerard          #+#    #+#             */
+/*   Updated: 2023/09/25 15:32:33 by stgerard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Identify.hpp"
+#include <iostream>
+#include <cstdlib> // Inclure la bibliothèque pour les fonctions de génération aléatoire
+#include <ctime>	// Inclure la bibliothèque pour l'initialisation de la graine
+
+int main()
+{
+
+
+	Base* base = generate();
+
+
+
+	return 0;
+}
