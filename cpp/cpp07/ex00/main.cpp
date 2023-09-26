1815a46d7cf97f47158bc71fe5cc293e25cc1d7e
stgerard <stgerard@student.42mulhouse.fr>
2023-09-26 12:00:17 +0200

    poiuy

--- cpp/cpp07/ex00/main.cpp
+++ cpp/cpp07/ex00/main.cpp
@@ -6,28 +6,30 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 09:49:57 by stgerard          #+#    #+#             */
-/*   Updated: 2023/09/26 10:55:36 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/09/26 11:59:57 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
+
 #include <iostream>
 
+int main( void ) {
+	int a = 2;
+	int b = 3;
 
-int main()
-{
-	int a = 3;
-	int b = 5;
-
-	swap(a, b);
+	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	
-	min(a, b);
-	std::cout << "min(a, b) = " << min(a, b) << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-	max(a, b);
-	std::cout << "max(a, b) = " << max(a, b) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
 
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
 	return 0;
 }
