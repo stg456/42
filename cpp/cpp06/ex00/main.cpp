b90a22eab320482e8075be4bde184e52094a79fc
stgerard <stgerard@student.42mulhouse.fr>
2023-10-04 11:11:25 +0200

    oiuuyt

--- cpp/cpp06/ex00/main.cpp
+++ cpp/cpp06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 09:49:57 by stgerard          #+#    #+#             */
-/*   Updated: 2023/09/22 11:36:19 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/04 11:09:38 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,18 @@ int main(int argc, char **argv)
 	}
 	if (argc == 2) {
 		if (!sc.check(argv[1])) {
-		std::cout << "char: impossible" << std::endl;
-		std::cout << "int: impossible" << std::endl;
-		std::cout << "float: nanf" << std::endl;
-		std::cout << "double: nan" << std::endl;
-		return 0;
+			std::cout << "char: impossible" << std::endl;
+			std::cout << "int: impossible" << std::endl;
+			std::cout << "float: nanf" << std::endl;
+			std::cout << "double: nan" << std::endl;
+			return 0;
+		}
+		else if (argv[1][0] == '0') {
+			std::cout << "char: Non displayable" << std::endl;
+			std::cout << "int: 0" << std::endl;
+			std::cout << "float: 0.0f" << std::endl;
+			std::cout << "double: 0.0" << std::endl;
+			return 0;
 		}
 		if (strlen(argv[1]) == 1 && !isdigit(argv[1][0]))
 			d = static_cast<double>(argv[1][0]);
