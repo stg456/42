b79e66577962ece35e9e5c70960230106610f074
stgerard <stgerard@student.42mulhouse.fr>
2023-10-04 11:44:29 +0200

    09oi87

--- cpp/cpp06/ex02/Identify.cpp
+++ cpp/cpp06/ex02/Identify.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 16:44:15 by stgerard          #+#    #+#             */
-/*   Updated: 2023/09/26 09:58:06 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/04 11:36:12 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,33 @@ void identify(Base* p) {
 }
 
 void identify(Base & p) {
-	if (dynamic_cast<A*>(&p))
-		std::cout << "A" << std::endl;
-	else if (dynamic_cast<B*>(&p))
-		std::cout << "B" << std::endl;
-	else if (dynamic_cast<C*>(&p))
-		std::cout << "C" << std::endl;
+	// if (dynamic_cast<A*>(&p))
+	// 	std::cout << "A" << std::endl;
+	// else if (dynamic_cast<B*>(&p))
+	// 	std::cout << "B" << std::endl;
+	// else if (dynamic_cast<C*>(&p))
+	// 	std::cout << "C" << std::endl;
+	try
+	{
+		A	&a_class = dynamic_cast<A&>(p);
+		std::cout << "Identify2: Class Type A" << std::endl;
+		(void) a_class;
+	}
+	catch(std::exception &e) {}
+
+	try
+	{
+		B	&b_class = dynamic_cast<B&>(p);
+		std::cout << "Identify2: Class Type B" << std::endl;
+		(void) b_class; 
+	}
+	catch(std::exception &e) {}
+
+	try
+	{
+		C	&c_class = dynamic_cast<C&>(p);
+		std::cout << "Identify2: Class Type C" << std::endl;
+		(void) c_class;
+	}
+	catch(std::exception &e) {}
 }
--- cpp/cpp06/ex02/main.cpp
+++ cpp/cpp06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 09:49:57 by stgerard          #+#    #+#             */
-/*   Updated: 2023/09/26 10:09:28 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/04 11:36:12 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ int main()
 	identify(base);
 
 	identify(*base);
+
+	delete base;
 
 	std::cout << std::endl;
 
