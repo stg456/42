112e16e386f8850508299a4569877b08cd3959d5
stgerard <stgerard@student.42mulhouse.fr>
2023-10-04 14:03:41 +0200

    kjht

--- cpp/cpp07/ex01/main.cpp
+++ cpp/cpp07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 09:49:57 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/04 13:45:58 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/04 14:02:39 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,31 @@
 
 #include <iostream>
 
-void print(int const & x) {
-	std::cout << x << std::endl;
+// void print(int const & x) {
+// 	std::cout << x << " ";
+// }
+
+// void print(std::string const & x) {
+// 	std::cout << x << " ";
+// }
+
+template <typename T> // neccessite :: pour l'instantiation
+void print(T const & x) {
+	std::cout << x << " ";
 }
 
 int main( void ) {
+	{
 	int array[5] = { 0, 1, 2, 3, 42 };
 	::iter(array, 5, &print);
 
 	std::cout << std::endl;
-
+	}
+	{
 	std::string array2[5] = { "hello", "42", "on", "the", "world" };
 	::iter(array2, 5, &print);
 
 	std::cout << std::endl;
-
+	}
 	return 0;
 }
