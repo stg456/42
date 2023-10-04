a4aae40f8e8164ea5281ade63280d13ec172652a
stgerard <stgerard@student.42mulhouse.fr>
2023-10-04 14:57:40 +0200

    ikujthr

--- cpp/cpp07/ex02/Array.hpp
+++ cpp/cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 14:08:28 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/04 14:17:21 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/04 14:57:36 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,42 @@
 template <typename T>
 class Array
 {
+	public:
+	Array<T>(): _size(0) {
+		_array = new T[0];
+	};
+
+	Array<T>(unsigned int n): _size(n - 1) {
+		_array = new T[n];
+	};
+
+	Array<T>(Array const & src) {
+		unsigned int size = src.size();
+		_size = size - 1;
+		_array = new T[size];
+
+		for (unsigned int i = 0; i < size; i++)
+			_array[i] = src._array[i];
+	};
+
+	~Array<T>() {
+		delete [] _array;
+	};
+
+	Array<T> & operator = (Array T const & rhs) {
+		for (size_t i = 0; i < _size; i++)
+			_array[i] = rhs._array[i];
+		return *this;
+	};
 
 
+
+	private:
+		T*				_array;
+		unsigned int	_size;
 };
+
+
 
 
 #endif
