35ab1821c9106d88089ce255c4ee664f97096228
stgerard <stgerard@student.42mulhouse.fr>
2023-10-05 17:11:51 +0200

    ytr

--- cpp/cpp07/ex02/main.cpp
+++ cpp/cpp07/ex02/main.cpp
@@ -6,12 +6,13 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 09:49:57 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/04 14:15:42 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/05 17:11:45 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <Array.hpp>
+
 
 #define MAX_VAL 750
 int main(int, char**)
