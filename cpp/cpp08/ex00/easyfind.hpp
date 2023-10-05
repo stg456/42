4a8a5dab4ecd566a1c26ab278eb74067d999dcc8
stgerard <stgerard@student.42mulhouse.fr>
2023-10-05 16:34:51 +0200

    oiuyt

--- cpp/cpp08/ex00/easyfind.hpp
+++ cpp/cpp08/ex00/easyfind.hpp
@@ -6,12 +6,20 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:14:42 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/05 16:17:17 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/05 16:34:45 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
+
+# include <iostream>
+# include <algorithm>
+# include <vector>
+# include <list>
+# include <map>
+
+
 
 template <typename T>
 
