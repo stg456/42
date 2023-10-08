fa94ff5beaa8c52c10304338f598849f423b8f0c
stgerard <stgerard@student.42mulhouse.fr>
2023-10-08 16:14:30 +0200

    juyhtgr

--- cpp/cpp08/ex00/main.cpp
+++ cpp/cpp08/ex00/main.cpp
@@ -6,33 +6,37 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:15:37 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/08 15:58:31 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/08 16:14:08 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
 int main() {
-	std::string allo[5] = {"allo", "la", "terre", "ici", "42"};
-	std::vector<int> nb;
 
+	std::vector<int> nb;
 	nb.push_back(2);
 	nb.push_back(1);
 	nb.push_back(4);
 	nb.push_back(5);
 	nb.push_back(3);
 
-	if (easyfind(nb, 3) == 1)
+	if (easyfind(nb, 1) == 1)
 		std::cout << "nb found" << std::endl;
 	else
 		std::cout << "nb not found" << std::endl;
 
+	// std::vector<std::string> allo;
+	// allo.push_back("allo");
+	// allo.push_back("la");
+	// allo.push_back("terre");
+	// allo.push_back("ici");
+	// allo.push_back("42");
 
-
-	if (easyfind(allo, "42") == 1)
-		std::cout << "42 found" << std::endl;
-	else
-		std::cout << "42 not found" << std::endl;
+	// if (easyfind(allo, "42") == 1)
+	// 	std::cout << "42 found" << std::endl;
+	// else
+	// 	std::cout << "42 not found" << std::endl;
 
 	return 0;
 }
