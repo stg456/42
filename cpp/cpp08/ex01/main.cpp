01a303b1e7dc21e01efe1c4adeea8c9a74fed1f1
stgerard <stgerard@student.42mulhouse.fr>
2023-10-08 16:45:57 +0200

    juhytg

--- cpp/cpp08/ex01/main.cpp
+++ cpp/cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:15:37 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/08 16:38:08 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/08 16:45:22 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,16 @@
 #include "Span.hpp"
 
 int main() {
+	Span sp = Span(5);
 
+	sp.addNumber(5);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
 
 	return 0;
 }
