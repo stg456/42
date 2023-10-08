ae91d436320212c83da3d51eec8b6770469b3b36
stgerard <stgerard@student.42mulhouse.fr>
2023-10-08 14:45:33 +0200

    ujyht

--- cpp/cpp08/ex00/easyfind.hpp
+++ cpp/cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:14:42 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/05 16:34:45 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/08 14:45:27 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,24 @@
 # define EASYFIND_HPP
 
 # include <iostream>
-// # include <stdexcept>
-// # include <algorithm>
+# include <stdexcept>
+# include <algorithm>
 # include <vector>
-// # include <iterator>
+# include <iterator>
+# include <list>
 
 
 template <typename T>
 void easyfind(T& container, int n) {
     typename T::iterator    iter;
-    for (iter = T&.begin; iter != T&.end; iter++) {
-        try {
 
-        }
-        catch std::exception {
-            return ("hors sol");
-        };
-    };
+		iter = std::find(container.begin(), container.end(), n);
+		if (iter != container.end())
+			std::cout << "found " << n << std::endl;
+		else
+			return ("not found");
+
+
 };
 
 
