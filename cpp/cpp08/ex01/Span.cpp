5c2648ea1b67f83f10d03afa168de296b98dd34a
stgerard <stgerard@student.42mulhouse.fr>
2023-10-09 11:20:39 +0200

    kjuyht

--- cpp/cpp08/ex01/Span.cpp
+++ cpp/cpp08/ex01/Span.cpp
@@ -6,13 +6,24 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 16:37:33 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/08 16:39:41 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/09 11:20:31 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
-Span::Span() : _size(0), _count(0), _array(NULL) {}
+Span::Span() : _size(0) {}
 
-Span::Span(unsigned int size) : _size(size), _count(0), _array(new int[size]) {}
+Span::Span(unsigned int size) : _size(size) {}
+
+Span::Span(const Span& copy) {
+	this->_size = copy._size;
+}
+
+Span::~Span() {}
+
+Span& Span::operator = (const Span& rhs) {
+	this->_size = rhs._size;
+	return *this;
+}
 
--- cpp/cpp08/ex01/Span.hpp
+++ cpp/cpp08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 16:34:06 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/08 16:37:20 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/09 11:18:00 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,22 @@ class Span
 {
 	private:
 		unsigned int		_size;
-		unsigned int		_count;
-		int*				_array;
 	
 	public:
 		Span();
 		Span(unsigned int size);
 		Span(const Span& copy);
 		~Span();
-		Span& operato = (const Span& rhs);
+		Span& operator = (const Span& rhs);
 		
 		void addNumber(int nb);
 		void addNumber(int nb, unsigned int count);
 		int shortestSpan();
 		int longestSpan();
+
+		class noSpanPossible : public std::exception {
+			virtual const char* what() const throw();
+		};
 };
 
 #endif
