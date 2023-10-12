505a1ca58e31fd519533ce174a5749ebe363aef4
stgerard <stgerard@student.42mulhouse.fr>
2023-10-12 11:13:48 +0200

    iujyhtr

--- cpp/cpp08/ex02/MutantStack.cpp
+++ cpp/cpp08/ex02/MutantStack.cpp
@@ -6,9 +6,15 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 17:31:14 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/10 17:45:46 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/12 11:13:41 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+
+MutantStack::MutantStack() : std::stack() {}
+
+MutantStack::MutantStack(const MutantStack& copy) : std::stack(copy) {}
+
+MutantStack::~MutantStack() {}
 
