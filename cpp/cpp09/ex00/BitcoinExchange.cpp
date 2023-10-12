f46c62d2776d1eb93de4a61e280d8e81c7b57d47
stgerard <stgerard@student.42.fr>
2023-10-12 15:57:22 +0200

    gtrfe

--- cpp/cpp09/ex00/BitcoinExchange.cpp
+++ cpp/cpp09/ex00/BitcoinExchange.cpp
@@ -1,2 +1,12 @@
 # include "BitcoinExchange.hpp"
 
+BitcoinExchange::BitcoinExchange(char *argv) {}
+
+BitcoinExchange::BitcoinExchange(BitcoinExchange const &src) {}
+
+BitcoinExchange::~BitcoinExchange() {}
+
+BitcoinExchange & BitcoinExchange::operator = (BitcoinExchange const &rhs) {}
+
+void	getData() {}
+
--- cpp/cpp09/ex00/BitcoinExchange.hpp
+++ cpp/cpp09/ex00/BitcoinExchange.hpp
@@ -5,6 +5,20 @@ BitcoinExchange.hpp
 
 # include <iostream>
 
+class BitcoinExchange 
+{
+	public:
+		BitcoinExchange(char *argv);
+		BitcoinExchange(BitcoinExchange const &src);
+		~BitcoinExchange();
 
+		BitcoinExchange & operator = (BitcoinExchange const &rhs);
+
+		void	getData();
+
+	private:
+		std::ifstream	data;
+		std::ifstream	input;
+}
 
 #endif
