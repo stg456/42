f9577a901946fbcc3742197e462941df20295b38
stgerard <stgerard@student.42mulhouse.fr>
2023-10-13 17:06:35 +0200

    ikjuyt

--- cpp/cpp09/ex00/BitcoinExchange.cpp
+++ cpp/cpp09/ex00/BitcoinExchange.cpp
@@ -14,14 +14,15 @@ BitcoinExchange::~BitcoinExchange() {}
 
 BitcoinExchange & BitcoinExchange::operator = (BitcoinExchange const &rhs) {
 	if (this != &rhs) {
-		this->data = rhs.data;
-		this->input = rhs.input;
+		this->dataMap = rhs.dataMap;
 	}
 	return *this;
 }
 
 void	getData() {
-	
+	for (std::map<std::string, float>::iterator it = dataMap.begin(); it != dataMap.end(); it++) {
+		std::cout << it->first << " => " << it->second << '\n';
+	}
 }
 
 const char* BitcoinExchange::notCorrectValueException::what() const throw() {
