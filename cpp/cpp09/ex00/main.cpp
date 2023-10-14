adc791e68191318b646f620f91e1bdefaf90c893
stgerard <stgerard@student.42mulhouse.fr>
2023-10-14 16:26:08 +0200

    ikjuhtrg

--- cpp/cpp09/ex00/main.cpp
+++ cpp/cpp09/ex00/main.cpp
@@ -6,14 +6,24 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:15:37 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/12 15:10:13 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/14 16:26:02 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int main() {
-
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		std::cout << "Error: need one arguments" << std::endl;
+		return 1;
+	}
+	try {
+		BitcoinExchange exchange(argv[1]);
+		exchange.getData();
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
 
 	return 0;
 }
