0f1d1dcdac811402371816bdb478e1f9bcde0dd5
stgerard <stgerard@student.42mulhouse.fr>
2023-10-25 16:33:22 +0200

    iujyhtgr

--- cpp/cpp09/ex00/main.cpp
+++ cpp/cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:15:37 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/25 16:08:15 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/25 16:31:12 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
 
 	BitcoinExchange btc;
 	btc.getData("data.csv");
-	// btc.getInput(argv[1]);
+	btc.getInput(argv[1]);
 
 
 
