3c16338f8ed21dd907aab2d4c364429c9e685112
stgerard <stgerard@student.42mulhouse.fr>
2023-10-26 14:24:09 +0200

    kiyujthg

--- cpp/cpp09/ex01/main.cpp
+++ cpp/cpp09/ex01/main.cpp
@@ -6,21 +6,20 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:15:37 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/17 16:41:50 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/26 14:23:50 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int main(int argc, char **argv) {
-	if (argc != 2) {
-		std::cout << "Error: could not open file" << std::endl;
-		return 1;
+int main(int ac, char **av) {
+	stack<std::string> rpn;
+	while (ac) {
+		rpn.push(av[ac - 1]);
+		ac--;
 	}
-	BitcoinExchange btc(argv[1]);
-	btc.getData("data.csv"); // load
 
-	btc.evalData(argv[1]); // eval
+
 
 
 
