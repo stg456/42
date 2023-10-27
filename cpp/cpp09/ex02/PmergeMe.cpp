00e18b30c402f7d6acc042184383c1a893ccfbfa
stgerard <stgerard@student.42mulhouse.fr>
2023-10-27 17:46:27 +0200

    ujyhtgr

--- cpp/cpp09/ex02/PmergeMe.cpp
+++ cpp/cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:42:10 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/27 17:13:35 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/27 17:46:00 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,15 @@ PmergeMe::PmergeMe(PmergeMe const &src) {
 }
 
 PmergeMe::~PmergeMe() {}
+
+
+PmergeMe & PmergeMe::operator = (PmergeMe const &rhs) {
+	if (this != &rhs) {
+		_vector = rhs._vector;
+	}
+	return *this;
+}
+
 
 bool PmergeMe::check(char *str) {
 	int i = 0;
