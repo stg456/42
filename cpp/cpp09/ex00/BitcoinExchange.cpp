67d0c5348b31d9fc41aca63d0187e201d756a667
stgerard <stgerard@student.42mulhouse.fr>
2023-10-27 16:18:03 +0200

    yujyhtrg

--- cpp/cpp09/ex00/BitcoinExchange.cpp
+++ cpp/cpp09/ex00/BitcoinExchange.cpp
@@ -6,34 +6,13 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 11:23:30 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/27 16:14:22 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/27 16:17:20 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
 BitcoinExchange::BitcoinExchange() {}
-
-// BitcoinExchange::BitcoinExchange(std::string filename) {}
-// 	std::ifstream infile(filename.c_str()); // c_str() converts string to char* with '\0'
-// 	if (!infile.is_open()) {
-// 		std::cout << "Error: could not open file" << std::endl;
-// 		return ;
-// 	}
-// 	std::string line;
-// 	while (std::getline(infile, line)) {
-// 		std::istringstream iss(line);
-// 		std::string date = line.substr(0, 10);
-// 		float price = std::stof(line.substr(11).c_str());
-// 		if (!(iss >> date >> price)) {
-// 			std::cout << "Error: could not read file" << std::endl;
-// 			return ;
-// 		}
-// 		_data.insert (std::pair<std::string, float>(date, price));
-// 		std::cout << "key: " << date << "value: " << price << std::endl;
-// 	}
-// 	infile.close();	
-// }
 
 BitcoinExchange::BitcoinExchange(BitcoinExchange const &src) {
 	*this = src;
@@ -103,30 +82,8 @@ void	BitcoinExchange::getInput(std::string input) {
 				std::cout << "key: " << str2.substr(0, 10) << std::fixed << std::setprecision(2) << " value: " << value * getPrice(str3) << std::endl;
 			}
 			check++;
-			// std::string date = str2.substr(0, 10);
-			// 	float price = std::stof(str2.substr(12));
-			// if (str2.size() < 11) {
-			// 	std::cout << "Error: bad input => " << str2 << std::endl;
-			// }
-			// // else if (!price)
-			// // 	std::cout << "Error: bad input => " << str2 << std::endl;
-			// if (price < 0)
-			// 	std::cout << "Error: not a positive number." << std::endl;
-			// else if (price > 1000)
-			// 	std::cout << "Error: too large a number." << std::endl;
-			
-			// _data.insert(std::make_pair(date, price));
-			// std::cout << "key: " << date << " value: " << price << std::endl;
-			// else
 		}
-		// check++;
 	}
-
-	// for (std::map<std::string, float>::iterator it = _data.begin(); it != _data.end(); it++) {
-		// std::cout << "key: " << it->first << " value: " << it->second << std::endl;
-		// validDate(it);
-		// std::cout << "key: " << it->first << " value: " << it->second << std::endl;
-	// }
 	file2.close();
 }
 
@@ -154,19 +111,6 @@ int BitcoinExchange::validDate(std::map<std::string, float>::iterator it) {
 	if ((m == 4 && d > 30) || (m == 6 && d > 30) || (m == 9 && d > 30) || (m == 11 && d > 30))
 		return (-1);
 	return 0;
-
-	// if ((year.length() != 4 || month.length() != 2 || day.length() != 2) || (year < "2009" || year > "2022")
-	// 	|| (month < "01" || month > "12") || (day < "01" || day > "31"))
-	// 	std::cout << "Error: bad input => " << it->first << std::endl;
-	// else if ((year == "2012" || year == "2016" || year == "2020") && (month == "02" && day > "29"))
-	// 	std::cout << "Error: bad input => " << it->first << std::endl;
-	// else if ((year != "2012" || year != "2016" || year != "2020") && (month == "02" && day > "28"))
-	// 	std::cout << "Error: bad input => " << it->first << std::endl;
-	// else if ((month == "04" && day > "30") || (month == "06" && day > "30") || (month == "09" && day > "30") || (month == "11" && day > "30"))
-	// 	std::cout << "Error: bad input => " << it->first << std::endl;
-	// else {
-	// 	std::cout << "key: " << it->first << " value: " << it->second << std::endl;
-	// }
 }
 
 float	BitcoinExchange::getPrice(std::string date) {
