236e857dd7222133ea7fc21d4071b6ff90ad01f8
stgerard <stgerard@student.42mulhouse.fr>
2023-10-30 12:55:41 +0100

    uikyjht

--- cpp/cpp09/ex02/PmergeMe.cpp
+++ cpp/cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:42:10 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/30 12:04:28 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/30 12:55:19 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,20 +32,20 @@ PmergeMe & PmergeMe::operator = (PmergeMe const &rhs) {
 
 int PmergeMe::check(char *str) {
 	int i = 0;
-	std::cout << "a1" << std::endl;
+	// std::cout << "a1" << std::endl;
 	while (str[i]) {
 		if (isdigit(str[i])) {
-			std::cout << str[i] << std::endl;
-			std::cout << isdigit(str[i]) << std::endl;
+			// std::cout << str[i] << std::endl;
+			// std::cout << isdigit(str[i]) << std::endl;
 			return 0;
 		}
 		else if (!isalpha(str[i])) {
-			std::cout << str[i] << std::endl;
+			// std::cout << str[i] << std::endl;
 			return 1;
 		}
 		i++;
 	}
-	std::cout << "a2" << std::endl;
+	// std::cout << "a2" << std::endl;
 	return 0;
 }
 
@@ -79,9 +79,9 @@ void PmergeMe::print(int ac, char **av) {
 // différence entre les deux contenants utilisés
 
 	
-	std::cout << "Before: ";
-	for (int i = 1; i <= ac; i++) {
-		std::cout << av[i];
+	std::cout << "Before:";
+	for (int i = 1; i < ac; i++) {
+		std::cout << " " << av[i];
 		// std::cout << _vector[i] << std::endl;
 		// std::cout << _deque[i] << std::endl;
 	}
@@ -95,15 +95,18 @@ void PmergeMe::print(int ac, char **av) {
 	std::sort(_deque.begin(), _deque.end());
 	std::clock_t end2 = std::clock();
 	
-	std::cout << "After: ";
+	std::cout << "After:";
 	std::vector<int>::iterator it = _vector.begin();
-
+	std::cout << " " << *it;
+	
+	std::deque<int>::iterator it2 = _deque.begin();
+	std::cout << " " << *it2;
 
 	while (it != _vector.end()) {
 		std::cout << *it << std::endl;
 		it++;
 	}
 
-	std::cout << "Time: " << (end1 - start1) * 1000 << std::endl;
-	std::cout << "Time: " << (end2 - start2) * 1000 << std::endl;
+	std::cout << "Time to process: " << (end1 - start1) * 1000 << std::endl;
+	std::cout << "Time to process: " << (end2 - start2) * 1000 << std::endl;
 }
--- cpp/cpp09/ex02/main.cpp
+++ cpp/cpp09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:15:37 by stgerard          #+#    #+#             */
-/*   Updated: 2023/10/30 12:02:00 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/10/30 12:47:19 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int main(int ac, char **av) {
 		std::cout << "Error: arguments required" << std::endl;
 		return 1;
 	}
-	std::cout << "a" << std::endl;
-	for (int i = 1; i <= ac; i++) {
+	// std::cout << "a" << std::endl;
+	for (int i = 1; i < ac; i++) {
 		if ((p.check(av[i]))) {
 			std::cout << "Error in arguments" << std::endl;
 			return 1;
 		}
 	}
-	std::cout << "b" << std::endl;
+	// std::cout << "b" << std::endl;
 	p.insert(av);
 	std::cout << "c" << std::endl;
 	p.print(ac, av);
