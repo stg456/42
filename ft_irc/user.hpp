f35baad5598734a649e55c3cca7fcac4e5845812
stgerard <stgerard@student.42mulhouse.fr>
2023-11-03 15:02:57 +0100

    tgre

--- ft_irc/user.hpp
+++ ft_irc/user.hpp
@@ -8,18 +8,35 @@
 class Server;
 
 class User {
+	private:
+		std::string		_userName;
+		std::string		_nickName;
+		std::string		_socket;
+		std::string		_password;
+		// Server			*_server;
+		// std::string		_channel;
+		std::string		_msg;
+		std::string		_state;
+
 	public:
 		User();
-		User(const User &src);
+		// User(const User &src);
 		User(int socket);
 		~User();
 		
-		std::string	getUserNmae();
-		std::string getNickName();
-		std::string getSocket();
+		std::string	getUserNmae() const;
+		std::string getNickName() const;
+		std::string getSocket() const;
+		std::string getState() const;
 		
 
 		void setPassword();
+
+		void setUserName(std::string userName);
+		void setNickName(std::string nickName);
+		void setSocket(std::string socket);
+		void setMsg(std::string msg);
+		void setState(std::string state);
 
 
 };
