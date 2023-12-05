90e764aa9dbbff3b89f37ff66e2ab0b256fa7769
stgerard <stgerard@student.42mulhouse.fr>
2023-12-05 15:00:28 +0100

    kjytr

--- exam05/ex00/Warlock.cpp
+++ exam05/ex00/Warlock.cpp
@@ -1,33 +1,37 @@
 #include "Warlock.hpp"
 
-Warlock::Warlock() {}
-
 Warlock::Warlock(std::string name, std::string title) : _name(name), _title(title) {
 	std::cout << _name << ": This looks like another boring day." << std::endl;
 }
 
-Warlock::Warlock(Warlock const &src) {
+Warlock::~Warlock() {
+	std::cout << _name << ": My job here is done!" << std::endl;
+}
+
+Warlock::Warlock() {}
+
+Warlock::Warlock(Warlock const & src) {
 	*this = src;
 }
 
 Warlock & Warlock::operator = (Warlock const & rhs) {
 	this->_name = rhs._name;
-	this->_title = rhs._title;
+	this->_titls = rhs._title;
 	return *this;
 }
 
-Warlock::~Warlock() {
-	std::cout << _name << ": My job is done!" << std::endl;
-}
-
-std::string const	Warlock::getName() {
+std::string const & Warlock::getName() const {
 	return (_name);
 }
 
-std::string	const	getTitle() {
+std::string const & Warlock::getTitle() const {
 	return (_title);
 }
 
-void			setTitles(const std::string const & title) {
+void Warlock::setTitle(std::string const & str) {
+	_title = str;
+}
 
+void introduce() const {
+	std::Cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;
 }
--- exam05/ex00/Warlock.hpp
+++ exam05/ex00/Warlock.hpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
-class Warlock
+class Warlock 
 {
 	private:
-		std::string	_name;
-		std::string	_title;
+		std::string _name;
+		std::string _title;
 		Warlock();
-		Warlock(Warlock const &src);
+		Warlock(Warlock const & src);
 		Warlock & operator = (Warlock const & rhs);
 
+
 	public:
-		Warlock();
 		Warlock(std::string name, std::string title);
 		~Warlock();
-		std::string	const	getName();
-		std::string	const	getTitle();
-		void			setTitles(const std::string const & title);
-		void			introduce() const;
+		std:string const & getName() const;
+		std::string const & getTitle() const;
+		void setTitle(std::string const & str);
+		void introduce() const;
 };
