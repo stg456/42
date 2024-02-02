7e43db186661f82440fc372be4029ca5e7916fbe
Stephane Gerard <stgerard@k0r4p2.42mulhouse.fr>
2024-02-02 15:54:52 +0100

    pffff

---
+++ Inception/srcs/requirements/wordpress/install_wordpress.sh
@@ -0,0 +1,10 @@
+if [! -f "/var/www/html/index.php"]; then #si index existe wp est installe
+    sleep 10
+    mkdir -p /var/www/html && chown -R www-data:www-data /var/www/html && chmod -R 755 /var/www/html # dossier site
+    mkdir -p /var/www/.wp-cli && chown -R www-data:www-data /var/www/.wp-cli && chmod -R 755 /var/www/.wp-cli # dossier wp-cli
+    sudo -u www-data wp core download --path="/var/www/html/"
+    mv /wp-config.php /var/www/html/wp-config.php
+    sudo -u www-data wp core install --path="/var/www/html/" --url=${WP_HOSTNAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
+    sudo -u www-data wp user create --path="/var/www/html/" ${WP_USER} ${WP_EMAIL} --role=author --user_pass=${WP_PASSWORD}
+fi
+php-fpm7.3 -F
