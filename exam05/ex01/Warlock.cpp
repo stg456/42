52ab10d2afb1c5a7d9d9624bfeaaf37e6e710fb8
Stephane Gerard <stgerard@k0r4p2.42mulhouse.fr>
2024-03-05 16:37:11 +0100

    ola

--- exam05/ex01/Warlock.cpp
+++ exam05/ex01/Warlock.cpp
@@ -19,6 +19,10 @@ Warlock::Warlock(std::string name, std::string title): _name(name), _title(title
 
 Warlock::~Warlock() {
     std::cout<<_name<<": My job here is done!"<<std::endl;
+    for (std::map<std::sting, ASpell*>::iterator it = _SpellBook.begin(); it != _SpellBook.end(); ++it) {
+        delete it->second;
+    }
+    _SpellBook.clear();
 }
 
 
@@ -36,5 +40,21 @@ void	Warlock::setTitle(std::string const &str) {
 
 void	Warlock::introduce() const {
     std::cout<<_name<<": I am "<<_name<<", "<<_title<<"!"<<std::endl;    
+}
+
+void Warlock::learnSpell(ASpell* spell) {
+    if (spell)
+        if (_SpellBook.find(spell.getName()) == _Spell.Book.end())
+            _SpellBook[spell->getName()] = spell->clone();
+}
+
+void Warlock::forgetSpell(std::string spellName) {
+    if (_spellBook.find(spellName) != _SpellBook.end())
+        _spellBook.erase(_SpellBook.find(spellName))
+}
+
+void Warlock::launchSpell(std::string spellName, ATarget const &target) {
+    if (_SpellBook.find(spellName) != _SpellBook.end())
+        _SpellBook[spellName]->launch(target);
 }
 
--- exam05/ex01/Warlock.hpp
+++ exam05/ex01/Warlock.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <iostream>
+#include <map>
+#include "ASpell"
 
 class Warlock
 {
     private:
         std::string _name;
         std::string _title;
+        std::map <std::string, ASpell*> _SpellBook;
 
         Warlock();
         Warlock(Warlock const &src);
@@ -20,5 +23,9 @@ class Warlock
         std::string const & getTitle() const;
         void setTitle(std::string const &str);
         void introduce() const;
+
+        void learnSpell(ASpell* spell);
+        void forgetSpell(std::string spellName);
+        void launchSpell(std::string spellName, ATarget const &target);
 };
 
