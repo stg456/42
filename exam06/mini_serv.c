5aa9de50f8a54908a794b3b7f294dc62d2f90e7e
Stephane Gerard <stgerard@k0r4p2.42mulhouse.fr>
2024-03-22 11:28:35 +0100

    start

--- exam06/mini_serv.c
+++ exam06/mini_serv.c
@@ -1,6 +1,41 @@
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/socket.h>
+
+typedef struct client
+{
+    int fd;
+    int id
+}           client;
+
+void err(char *str)
+{
+    write(2, str, strlen(str));
+    exit (1);
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+        err("Wrong number of arguments\n");
+// init const
+
+// verif err fatal
+
+// creer le socket
+
+// config adresse serv
+
+// bind du socket a l'adresse
+
+// le socket ecoute
+
+// init descripteur de fichier
+
+// boucle du serveur
 
 
+
+}
