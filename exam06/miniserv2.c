b9506a8d4f8a86c511b191147f116c69db177c46
Stephane Gerard <stgerard@k0r4p2.42mulhouse.fr>
2024-03-26 15:46:19 +0100

    exam06

---
+++ exam06/miniserv2.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netdb.h>
+#include <netinet/in.h>
+
+
+typedef struct s_client
+{
+    int id;
+    char msg[1024];
+}           t_client;
+
+t_client clients[1024];
+fd_set rfd, wfd, act;
+int maxfd = 0, nextid = 0;
+char rbuf[100000];
+char wbuf[100000];
+
+void err(char *msg)
+{
+    if (msg)
+        write(2, msg, strlen(msg));
+    else
+        write(2, "Fatal error", 11);
+    write(2, '\n', 1);
+    exit (1);    
+}
+
+void sendAll(int senderfd)
+{
+    for (int fd; fd <= maxfd; fd++)
+        if (FD_ISSET(fd, &wfd) && fd != senderfd)
+            send (fd, &wbuf, strlen(wbuf), 0);
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+        err("Wrong number of arguments\n");
+
+    
+}
